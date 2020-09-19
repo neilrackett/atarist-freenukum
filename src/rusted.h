b63ee98cd9b9520e1864456ac0a287b698ff13e9
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-09-19 07:33:58 +0200

    Build src/rusted.h C FFI header from Rust

---
+++ src/rusted.h
@@ -0,0 +1,22 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+
+static const uintptr_t FONT_HEIGHT = 8;
+
+static const uintptr_t FONT_WIDTH = 8;
+
+static const uintptr_t HALFTILE_HEIGHT = 8;
+
+static const uintptr_t HALFTILE_WIDTH = 8;
+
+static const uintptr_t HEALTH_COUNT = 8;
+
+static const uintptr_t INVENTORY_WIDTH = (HEALTH_COUNT / 2);
+
+static const uintptr_t MAX_TILES_PER_FILE = 50;
+
+static const uintptr_t TILE_HEIGHT = (HALFTILE_HEIGHT * 2);
+
+static const uintptr_t TILE_WIDTH = (HALFTILE_WIDTH * 2);
