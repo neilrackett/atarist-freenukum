a6dfbc263db7f78a7e40b66d72ad44f3bcc0d33d
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-10 21:27:25 +0200

    Fix file naming of robot and wallcrawler

--- src/transition/level/actor/robot.rs
+++ src/transition/level/actor/robot.rs
@@ -1,19 +1,15 @@
-use super::super::super::{HorizontalDirection, VerticalDirection};
+use super::super::super::HorizontalDirection;
 
 struct Specific {
-    direction: VerticalDirection,
-    orientation: HorizontalDirection,
+    direction: HorizontalDirection,
     tile: usize,
     current_frame: usize,
     num_frames: usize,
-    was_shot: bool,
     touching_hero: bool,
 }
 
 pub mod ffi {
-    use super::super::super::super::{
-        HorizontalDirection, VerticalDirection,
-    };
+    use super::super::super::super::HorizontalDirection;
     use super::super::ffi::{
         FnLevelActorActParams, FnLevelActorBlitParams,
         FnLevelActorCreateParams, FnLevelActorFreeParams,
@@ -23,13 +19,13 @@ pub mod ffi {
     use super::super::{ActorQueueItem, ActorType};
     use super::Specific;
     use crate::{
-        ANIMATION_WALLCRAWLERBOT_LEFT, ANIMATION_WALLCRAWLERBOT_RIGHT,
-        HALFTILE_HEIGHT, HALFTILE_WIDTH, TILE_HEIGHT, TILE_WIDTH,
+        ANIMATION_ROBOT, HALFTILE_HEIGHT, HALFTILE_WIDTH, TILE_HEIGHT,
+        TILE_WIDTH,
     };
     use transdl::video::Surface;
 
     #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_wallcrawler_create(
+    pub extern "C" fn fn_level_actor_function_robot_create(
         p: FnLevelActorCreateParams,
     ) {
         assert!(!p.general.is_null());
@@ -39,26 +35,12 @@ pub mod ffi {
 
         general.position.w = TILE_WIDTH as u16;
         general.position.h = TILE_HEIGHT as u16;
-        general.is_in_foreground = true;
-
-        let (tile, orientation) = match general.actor_type {
-            ActorType::WallCrawlerBotLeft => {
-                (ANIMATION_WALLCRAWLERBOT_LEFT, HorizontalDirection::Left)
-            }
-            ActorType::WallCrawlerBotRight => (
-                ANIMATION_WALLCRAWLERBOT_RIGHT,
-                HorizontalDirection::Right,
-            ),
-            _ => unreachable!(),
-        };
 
         let data = Box::new(Specific {
-            direction: VerticalDirection::Up,
-            orientation,
-            tile,
+            direction: HorizontalDirection::Left,
+            tile: ANIMATION_ROBOT,
             current_frame: 0,
-            num_frames: 4,
-            was_shot: false,
+            num_frames: 3,
             touching_hero: false,
         });
 
@@ -66,7 +48,7 @@ pub mod ffi {
     }
 
     #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_wallcrawler_free(
+    pub extern "C" fn fn_level_actor_function_robot_free(
         p: FnLevelActorFreeParams,
     ) {
         unsafe {
@@ -77,7 +59,7 @@ pub mod ffi {
     }
 
     #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_wallcrawler_hero_touch_start(
+    pub extern "C" fn fn_level_actor_function_robot_hero_touch_start(
         p: FnLevelActorHeroTouchStartParams,
     ) {
         assert!(!p.general.is_null());
@@ -90,7 +72,7 @@ pub mod ffi {
     }
 
     #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_wallcrawler_hero_touch_end(
+    pub extern "C" fn fn_level_actor_function_robot_hero_touch_end(
         p: FnLevelActorHeroTouchEndParams,
     ) {
         assert!(!p.general.is_null());
@@ -103,7 +85,7 @@ pub mod ffi {
     }
 
     #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_wallcrawler_act(
+    pub extern "C" fn fn_level_actor_function_robot_act(
         p: FnLevelActorActParams,
     ) {
         assert!(!p.general.is_null());
@@ -113,74 +95,65 @@ pub mod ffi {
         let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
         let level_data = unsafe { &mut (*p.level_data) };
 
-        let direction = match specific.direction {
-            VerticalDirection::Up => 1,
-            VerticalDirection::Down => -1,
-            VerticalDirection::Center => unreachable!(),
-        };
-        let orientation = match specific.orientation {
-            HorizontalDirection::Left => -1,
-            HorizontalDirection::Right => 1,
-            HorizontalDirection::Center => unreachable!(),
-        };
+        specific.current_frame += 1;
+        specific.current_frame %= specific.num_frames;
 
-        if direction > 0 {
-            // going up
-            specific.current_frame += 1;
-            specific.current_frame %= specific.num_frames;
-
-            if
-            // bot collides with solid tile
-            level_data.solids.get(
-                general.position.x as usize / TILE_WIDTH,
-                (general.position.y as usize - 1) / TILE_WIDTH) ||
-            // bot has no more wall to stick upon
-            !level_data.solids.get(
-                (
-                    general.position.x as isize +
-                    orientation as isize *
-                    TILE_WIDTH as isize
-                ) as usize / TILE_WIDTH,
-                (general.position.y as usize - 1) / TILE_HEIGHT)
-            {
-                general.position.y += 1;
-                specific.direction = VerticalDirection::Down;
-            } else {
-                general.position.y -= 1;
-            }
+        if !level_data.solids.get(
+            general.position.x as usize / TILE_WIDTH,
+            general.position.y as usize / TILE_HEIGHT + 1,
+        ) {
+            // In the air, falling down.
+            general.position.y += HALFTILE_HEIGHT as i16;
         } else {
-            // going down
+            // On the floor, walking.
             if specific.current_frame == 0 {
-                specific.current_frame = specific.num_frames;
-            }
-            specific.current_frame -= 1;
-
-            if
-            // bot collides with solid tile
-            level_data.solids.get(
-                    general.position.x as usize / TILE_WIDTH,
-                    (
-                        general.position.y as usize + TILE_HEIGHT
-                    ) / TILE_HEIGHT) ||
-            // bot has no more wall to stick upon
-            !level_data.solids.get(
+                let mut direction = match specific.direction {
+                    HorizontalDirection::Left => -1,
+                    HorizontalDirection::Right => 2,
+                    HorizontalDirection::Center => unreachable!(),
+                };
+                // Check if the place next to the bot is free
+                if !level_data.solids.get(
                 (
                     general.position.x as isize +
-                    orientation as isize *
-                    TILE_WIDTH as isize) as usize /
-                TILE_WIDTH,
-                (general.position.y as usize + TILE_HEIGHT) / TILE_HEIGHT)
-            {
-                general.position.y -= 1;
-                specific.direction = VerticalDirection::Up;
-            } else {
-                general.position.y += 1;
+                    direction * HALFTILE_WIDTH as isize
+                ) as usize/ TILE_WIDTH,
+                general.position.y as usize / TILE_HEIGHT
+            ) &&
+            // Check if the tile below this free place is solid
+            level_data.solids.get(
+                (
+                    general.position.x as isize +
+                    direction * HALFTILE_WIDTH as isize
+                ) as usize / TILE_WIDTH,
+                (general.position.y as usize + TILE_HEIGHT) / TILE_HEIGHT
+            ) {
+                    if direction == 2 {
+                        direction = 1;
+                    }
+                    general.position.x +=
+                        direction as i16 * HALFTILE_WIDTH as i16;
+                } else {
+                    specific.direction = if specific.direction
+                        == HorizontalDirection::Left
+                    {
+                        HorizontalDirection::Right
+                    } else {
+                        HorizontalDirection::Left
+                    };
+                    if direction == 2 {
+                        direction = 1
+                    };
+                    direction *= -1;
+                    general.position.x +=
+                        direction as i16 * HALFTILE_WIDTH as i16;
+                }
             }
         }
     }
 
     #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_wallcrawler_blit(
+    pub extern "C" fn fn_level_actor_function_robot_blit(
         p: FnLevelActorBlitParams,
     ) {
         assert!(!p.general.is_null());
@@ -194,15 +167,13 @@ pub mod ffi {
 
         let mut target = Surface { raw: target };
 
-        let tile = tilecache
-            .get_tile(specific.tile + specific.current_frame)
-            .unwrap();
+        let tile = tilecache.get_tile(specific.tile as usize).unwrap();
         let destrect = general.position;
         tile.blit_to_sdl_surface(None, &mut target, Some(destrect));
     }
 
     #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_wallcrawler_shot(
+    pub extern "C" fn fn_level_actor_function_robot_shot(
         p: FnLevelActorShotParams,
     ) {
         assert!(!p.general.is_null());
@@ -214,25 +185,16 @@ pub mod ffi {
         let hero_data = unsafe { &mut (*p.hero_data) };
         let actor_queue = unsafe { &mut (*p.actor_queue) };
 
-        if !specific.was_shot {
-            if specific.touching_hero {
-                general.hurts_hero = false;
-                specific.touching_hero = false;
-                specific.current_frame = 0;
-            }
-            general.is_alive = false;
-
-            hero_data.score.add(100);
-            actor_queue.push_back(ActorQueueItem {
-                actor_type: ActorType::Steam,
-                x: general.position.x as u16,
-                y: general.position.y as u16,
-            });
-            actor_queue.push_back(ActorQueueItem {
-                actor_type: ActorType::Explosion,
-                x: general.position.x as u16,
-                y: general.position.y as u16,
-            });
+        hero_data.score.add(100);
+        if specific.touching_hero {
+            general.hurts_hero = false;
+            specific.touching_hero = false;
         }
+        actor_queue.push_back(ActorQueueItem {
+            actor_type: ActorType::RobotDisappearing,
+            x: general.position.x as u16,
+            y: general.position.y as u16,
+        });
+        general.is_alive = false;
     }
 }
