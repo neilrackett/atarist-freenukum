8d9323668d1467fcdde6687cad4077646f934e46
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-13 18:16:09 +0200

    Fix naming of redball_jumping and simpleanimation files

--- src/transition/level/actor/simpleanimation.rs
+++ src/transition/level/actor/simpleanimation.rs
@@ -1,23 +1,24 @@
 struct Specific {
-    tile: usize,
-    touching_hero: u8,
+    tile: u16,
+    current_frame: u16,
+    num_frames: u16,
 }
 
 pub mod ffi {
     use super::super::ffi::{
         FnLevelActorActParams, FnLevelActorBlitParams,
         FnLevelActorCreateParams, FnLevelActorFreeParams,
-        FnLevelActorHeroTouchStartParams, FnLevelActorShotParams,
     };
     use super::super::ActorType;
     use super::Specific;
     use crate::{
-        ANIMATION_MINE, HALFTILE_HEIGHT, TILE_HEIGHT, TILE_WIDTH,
+        ANIMATION_BROKENWALLBG, ANIMATION_STONEWINDOWBG,
+        ANIMATION_WINDOWBG, TILE_HEIGHT, TILE_WIDTH,
     };
     use transdl::video::Surface;
 
     #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_redball_lying_create(
+    pub extern "C" fn fn_level_actor_function_simpleanimation_create(
         p: FnLevelActorCreateParams,
     ) {
         assert!(!p.general.is_null());
@@ -25,19 +26,56 @@ pub mod ffi {
         let general = unsafe { &mut (*p.general) };
         let specific = unsafe { &mut (*p.specific) };
 
+        general.is_in_foreground = false;
         general.position.w = TILE_WIDTH as u16;
         general.position.h = TILE_HEIGHT as u16;
 
+        let (tile, num_frames) = match general.actor_type {
+            ActorType::TextOnScreenBackground => (0x0004, 4),
+            ActorType::HighVoltageFlashBackground => (0x0008, 4),
+            ActorType::RedFlashlightBackground => (0x000C, 4),
+            ActorType::BlueFlashlightBackground => (0x0010, 4),
+            ActorType::KeypanelBackground => (0x0014, 4),
+            ActorType::RedRotationLightBackground => (0x0018, 4),
+            ActorType::UpArrowBackground => (0x001C, 4),
+            ActorType::BlueLightBackground1 => (0x0020, 4),
+            ActorType::BlueLightBackground2 => (0x0021, 4),
+            ActorType::BlueLightBackground3 => (0x0022, 4),
+            ActorType::BlueLightBackground4 => (0x0023, 4),
+            ActorType::GreenPoisonBackground => (0x0028, 4),
+            ActorType::LavaBackground => (0x002C, 4),
+            ActorType::WindowLeftBackground => {
+                (ANIMATION_WINDOWBG as u16, 1)
+            }
+            ActorType::WindowRightBackground => {
+                (ANIMATION_WINDOWBG as u16 + 1, 1)
+            }
+            ActorType::StoneWindowBackground => {
+                (ANIMATION_STONEWINDOWBG as u16, 1)
+            }
+            ActorType::BrokenWallBackground => {
+                (ANIMATION_BROKENWALLBG as u16, 1)
+            }
+            _ => {
+                unreachable!(
+                    "Actor type {:?} added as an animation \
+                    which is not an animation id",
+                    general.actor_type
+                );
+            }
+        };
+
         let data = Box::new(Specific {
-            tile: ANIMATION_MINE,
-            touching_hero: 0,
+            tile,
+            current_frame: 0,
+            num_frames,
         });
 
         *specific = Box::into_raw(data) as *mut libc::c_void;
     }
 
     #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_redball_lying_free(
+    pub extern "C" fn fn_level_actor_function_simpleanimation_free(
         p: FnLevelActorFreeParams,
     ) {
         unsafe {
@@ -48,55 +86,18 @@ pub mod ffi {
     }
 
     #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_redball_lying_hero_touch_start(
-        p: FnLevelActorHeroTouchStartParams,
-    ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-
-        general.hurts_hero = true;
-        specific.touching_hero = 1;
-    }
-
-    #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_redball_lying_act(
+    pub extern "C" fn fn_level_actor_function_simpleanimation_act(
         p: FnLevelActorActParams,
     ) {
-        assert!(!p.general.is_null());
         assert!(!p.specific.is_null());
-        assert!(!p.level_data.is_null());
-        assert!(!p.actor_queue.is_null());
-        let general = unsafe { &mut (*p.general) };
         let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        let level_data = unsafe { &mut (*p.level_data) };
-        let actor_queue = unsafe { &mut (*p.actor_queue) };
 
-        if !level_data.solids.get(
-            general.position.x as usize / TILE_WIDTH,
-            general.position.y as usize / TILE_HEIGHT + 1,
-        ) {
-            general.position.y += HALFTILE_HEIGHT as i16;
-        }
-
-        match specific.touching_hero {
-            1 => specific.touching_hero += 1,
-            2 => {
-                general.hurts_hero = false;
-                general.is_alive = false;
-                actor_queue.push_back(
-                    ActorType::Fire,
-                    general.position.x as u16,
-                    general.position.y as u16,
-                );
-            }
-            _ => {}
-        }
+        specific.current_frame += 1;
+        specific.current_frame %= specific.num_frames;
     }
 
     #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_redball_lying_blit(
+    pub extern "C" fn fn_level_actor_function_simpleanimation_blit(
         p: FnLevelActorBlitParams,
     ) {
         assert!(!p.general.is_null());
@@ -110,18 +111,10 @@ pub mod ffi {
 
         let mut target = Surface { raw: target };
 
-        let tile = tilecache.get_tile(specific.tile as usize).unwrap();
+        let tile = tilecache
+            .get_tile((specific.tile + specific.current_frame) as usize)
+            .unwrap();
         let destrect = general.position;
         tile.blit_to_sdl_surface(None, &mut target, Some(destrect));
-    }
-
-    #[no_mangle]
-    pub extern "C" fn fn_level_actor_function_redball_lying_shot(
-        _p: FnLevelActorShotParams,
-    ) {
-        /*
-         * We don't need to do anything, this is just to absorb
-         * the bullet when the actor is shot.
-         */
     }
 }
