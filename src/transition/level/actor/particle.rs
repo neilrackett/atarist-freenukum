bdff315cce50241c6fd8cc5386f3b25d44224610
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-16 16:13:51 +0200

    Move particle functionality out of ffi

--- src/transition/level/actor/particle.rs
+++ src/transition/level/actor/particle.rs
@@ -1,3 +1,14 @@
+use super::super::super::hero::HeroData;
+use super::super::super::tilecache::TileCache;
+use super::super::LevelData;
+use super::{ActorData, ActorQueue, ActorType};
+use crate::{
+    HALFTILE_HEIGHT, HALFTILE_WIDTH, OBJECT_SPARK_BLUE,
+    OBJECT_SPARK_GREEN, OBJECT_SPARK_PINK, OBJECT_SPARK_WHITE,
+};
+use transdl::video::Surface;
+
+#[derive(Debug)]
 struct Specific {
     tile: usize,
     countdown: usize,
@@ -5,102 +16,95 @@ struct Specific {
     vspeed: i16,
 }
 
+fn create(
+    general: &mut ActorData,
+    _level_data: &mut LevelData,
+) -> Specific {
+    general.is_in_foreground = true;
+    general.position.w = HALFTILE_WIDTH as u16;
+    general.position.h = HALFTILE_HEIGHT as u16;
+
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    let vspeed = rng.gen_range(-12, 5);
+    let hspeed = rng.gen_range(-8, 9);
+
+    let tile = match general.actor_type {
+        ActorType::ParticlePink => OBJECT_SPARK_PINK,
+        ActorType::ParticleBlue => OBJECT_SPARK_BLUE,
+        ActorType::ParticleWhite => OBJECT_SPARK_WHITE,
+        ActorType::ParticleGreen => OBJECT_SPARK_GREEN,
+        _ => unreachable!(),
+    };
+
+    Specific {
+        tile,
+        countdown: 20,
+        hspeed,
+        vspeed,
+    }
+}
+
+fn act(
+    general: &mut ActorData,
+    specific: &mut Specific,
+    _level_data: &mut LevelData,
+    _actor_queue: &mut ActorQueue,
+    _hero_data: &mut HeroData,
+) {
+    if specific.countdown > 0 {
+        specific.countdown -= 1;
+        general.position.x += specific.hspeed;
+        general.position.y += specific.vspeed;
+        specific.vspeed += 2;
+    } else {
+        general.is_alive = false;
+    }
+}
+
+fn blit(
+    general: &mut ActorData,
+    specific: &mut Specific,
+    _hero_data: &mut HeroData,
+    tilecache: &TileCache,
+    target: &mut Surface,
+) {
+    let tile = tilecache.get_tile((specific.tile) as usize).unwrap();
+    let destrect = general.position;
+    tile.blit_to_sdl_surface(None, target, Some(destrect));
+}
+
 pub mod ffi {
     use super::super::ffi::{
         FnLevelActorActParams, FnLevelActorBlitParams,
         FnLevelActorCreateParams, FnLevelActorFreeParams,
     };
-    use super::super::ActorType;
-    use super::Specific;
-    use crate::{
-        HALFTILE_HEIGHT, HALFTILE_WIDTH, OBJECT_SPARK_BLUE,
-        OBJECT_SPARK_GREEN, OBJECT_SPARK_PINK, OBJECT_SPARK_WHITE,
-    };
-    use transdl::video::Surface;
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_particle_create(
         p: FnLevelActorCreateParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*p.specific) };
-
-        general.is_in_foreground = true;
-        general.position.w = HALFTILE_WIDTH as u16;
-        general.position.h = HALFTILE_HEIGHT as u16;
-
-        use rand::Rng;
-        let mut rng = rand::thread_rng();
-        let vspeed = rng.gen_range(-12, 5);
-        let hspeed = rng.gen_range(-8, 9);
-
-        let tile = match general.actor_type {
-            ActorType::ParticlePink => OBJECT_SPARK_PINK,
-            ActorType::ParticleBlue => OBJECT_SPARK_BLUE,
-            ActorType::ParticleWhite => OBJECT_SPARK_WHITE,
-            ActorType::ParticleGreen => OBJECT_SPARK_GREEN,
-            _ => unreachable!(),
-        };
-
-        let data = Box::new(Specific {
-            tile,
-            countdown: 20,
-            hspeed,
-            vspeed,
-        });
-
-        *specific = Box::into_raw(data) as *mut libc::c_void;
+        p.call(super::create);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_particle_free(
         p: FnLevelActorFreeParams,
     ) {
-        unsafe {
-            if !(*p.specific).is_null() {
-                Box::from_raw(*p.specific);
-            }
-        }
+        p.call::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_particle_act(
         p: FnLevelActorActParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        let general = unsafe { &mut (*(p.general)) };
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-
-        if specific.countdown > 0 {
-            specific.countdown -= 1;
-            general.position.x += specific.hspeed;
-            general.position.y += specific.vspeed;
-            specific.vspeed += 2;
-        } else {
-            general.is_alive = false;
-        }
+        p.call(super::act);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_particle_blit(
         p: FnLevelActorBlitParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        assert!(!p.tilecache.is_null());
-        assert!(!p.target.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        let tilecache = unsafe { &(*p.tilecache) };
-        let target = unsafe { &mut (*p.target) };
-
-        let mut target = Surface { raw: target };
-
-        let tile = tilecache.get_tile((specific.tile) as usize).unwrap();
-        let destrect = general.position;
-        tile.blit_to_sdl_surface(None, &mut target, Some(destrect));
+        p.call(super::blit);
     }
 }
