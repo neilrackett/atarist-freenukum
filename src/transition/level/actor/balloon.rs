3a629c11614c3c63dce391b5f49840380131a317
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-16 18:50:17 +0200

    Move balloon functions into ActorInterface trait

--- src/transition/level/actor/balloon.rs
+++ src/transition/level/actor/balloon.rs
@@ -1,7 +1,7 @@
 use super::super::super::hero::HeroData;
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
-use super::{ActorData, ActorQueue, ActorType};
+use super::{ActorData, ActorInterface, ActorQueue, ActorType};
 use crate::{OBJECT_BALLOON, TILE_HEIGHT, TILE_WIDTH};
 use transdl::video::Surface;
 
@@ -11,104 +11,106 @@ struct Specific {
     current_frame: usize,
 }
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16 * 2;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Self {
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16 * 2;
 
-    Specific {
-        destroyed: false,
-        current_frame: 0,
+        Specific {
+            destroyed: false,
+            current_frame: 0,
+        }
     }
-}
 
-fn hero_touch_start(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    actor_queue: &mut ActorQueue,
-    hero_data: &mut HeroData,
-) {
-    if !specific.destroyed {
-        general.is_alive = false;
-        hero_data.score.add(10000);
-        actor_queue.push_back(
-            ActorType::Score10000,
-            general.position.x as u16,
-            general.position.y as u16,
-        );
-    }
-}
-fn act(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    level_data: &mut LevelData,
-    actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    specific.current_frame += 1;
-    specific.current_frame %= 9;
-
-    if specific.destroyed {
-        general.is_alive = false;
-    } else {
-        general.position.y -= 1;
-        if level_data.solids.get(
-            general.position.x as usize / TILE_WIDTH,
-            general.position.y as usize / TILE_WIDTH,
-        ) {
-            // balloon bumps against wall
-            specific.destroyed = true;
+    fn hero_touch_start(
+        &mut self,
+        general: &mut ActorData,
+        actor_queue: &mut ActorQueue,
+        hero_data: &mut HeroData,
+    ) {
+        if !self.destroyed {
+            general.is_alive = false;
+            hero_data.score.add(10000);
             actor_queue.push_back(
-                ActorType::Steam,
+                ActorType::Score10000,
                 general.position.x as u16,
                 general.position.y as u16,
             );
         }
     }
-}
+    fn act(
+        &mut self,
+        general: &mut ActorData,
+        level_data: &mut LevelData,
+        actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        self.current_frame += 1;
+        self.current_frame %= 9;
 
-fn blit(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    let mut destrect = general.position;
+        if self.destroyed {
+            general.is_alive = false;
+        } else {
+            general.position.y -= 1;
+            if level_data.solids.get(
+                general.position.x as usize / TILE_WIDTH,
+                general.position.y as usize / TILE_WIDTH,
+            ) {
+                // balloon bumps against wall
+                self.destroyed = true;
+                actor_queue.push_back(
+                    ActorType::Steam,
+                    general.position.x as u16,
+                    general.position.y as u16,
+                );
+            }
+        }
+    }
 
-    let tile = if specific.destroyed {
-        OBJECT_BALLOON + 4
-    } else {
-        OBJECT_BALLOON
-    };
-    tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
-        None,
-        target,
-        Some(destrect),
-    );
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        let mut destrect = general.position;
 
-    destrect.y += TILE_HEIGHT as i16;
-    tilecache
-        .get_tile(OBJECT_BALLOON + 1 + specific.current_frame / 3)
-        .unwrap()
-        .blit_to_sdl_surface(None, target, Some(destrect));
-}
+        let tile = if self.destroyed {
+            OBJECT_BALLOON + 4
+        } else {
+            OBJECT_BALLOON
+        };
+        tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
+            None,
+            target,
+            Some(destrect),
+        );
 
-fn shot(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _level_data: &mut LevelData,
-    actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    specific.destroyed = true;
-    actor_queue.push_back(
-        ActorType::Steam,
-        general.position.x as u16,
-        general.position.y as u16,
-    );
+        destrect.y += TILE_HEIGHT as i16;
+        tilecache
+            .get_tile(OBJECT_BALLOON + 1 + self.current_frame / 3)
+            .unwrap()
+            .blit_to_sdl_surface(None, target, Some(destrect));
+    }
+
+    fn shot(
+        &mut self,
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+        actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        self.destroyed = true;
+        actor_queue.push_back(
+            ActorType::Steam,
+            general.position.x as u16,
+            general.position.y as u16,
+        );
+    }
 }
 
 pub mod ffi {
@@ -122,41 +124,41 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_balloon_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_balloon_free(
         p: FnLevelActorFreeParams,
     ) {
-        p.call::<super::Specific>();
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_balloon_hero_touch_start(
         p: FnLevelActorHeroTouchStartParams,
     ) {
-        p.call(super::hero_touch_start);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_balloon_act(
         p: FnLevelActorActParams,
     ) {
-        p.call(super::act);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_balloon_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_balloon_shot(
         p: FnLevelActorShotParams,
     ) {
-        p.call(super::shot);
+        p.call_interface::<super::Specific>();
     }
 }
