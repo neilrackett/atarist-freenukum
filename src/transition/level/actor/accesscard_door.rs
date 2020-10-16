05916199c064ac1169cfa4bcf9e307731c8eb0c8
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-16 13:21:16 +0200

    Move accesscard_door functionality out of ffi

--- src/transition/level/actor/accesscard_door.rs
+++ src/transition/level/actor/accesscard_door.rs
@@ -1,8 +1,71 @@
+use super::super::super::hero::HeroData;
+use super::super::super::tilecache::TileCache;
+use super::super::LevelData;
+use super::ActorMessageType;
+use super::{ActorData, ActorQueue};
+use crate::{OBJECT_LASERBEAM, TILE_HEIGHT, TILE_WIDTH};
+use transdl::video::Surface;
+
 #[derive(Debug)]
 struct Specific {
     tile: usize,
     current_frame: usize,
     num_frames: usize,
+}
+
+fn create(
+    general: &mut ActorData,
+    _level_data: &mut LevelData,
+) -> Specific {
+    general.position.w = TILE_WIDTH as u16;
+    general.position.h = TILE_HEIGHT as u16;
+    general.is_in_foreground = false;
+
+    Specific {
+        tile: OBJECT_LASERBEAM,
+        current_frame: 0,
+        num_frames: 4,
+    }
+}
+
+fn act(
+    _general: &mut ActorData,
+    specific: &mut Specific,
+    _level_data: &mut LevelData,
+    _actor_queue: &mut ActorQueue,
+    _hero_data: &mut HeroData,
+) {
+    specific.current_frame += 1;
+    specific.current_frame %= specific.num_frames;
+}
+
+fn blit(
+    general: &mut ActorData,
+    specific: &mut Specific,
+    _hero_data: &mut HeroData,
+    tilecache: &TileCache,
+    target: &mut Surface,
+) {
+    tilecache
+        .get_tile(specific.tile + specific.current_frame)
+        .unwrap()
+        .blit_to_sdl_surface(None, target, Some(general.position));
+}
+
+fn receive_message(
+    general: &mut ActorData,
+    _specific: &mut Specific,
+    message: ActorMessageType,
+    _hero_data: &mut HeroData,
+    level_data: &mut LevelData,
+) {
+    if message != ActorMessageType::OpenDoor {
+        return;
+    }
+    let x = general.position.x as usize / TILE_WIDTH;
+    let y = general.position.y as usize / TILE_HEIGHT;
+    level_data.solids.set(x, y, false);
+    general.is_alive = false;
 }
 
 pub mod ffi {
@@ -11,95 +74,40 @@ pub mod ffi {
         FnLevelActorCreateParams, FnLevelActorFreeParams,
         FnLevelActorReceiveMessageParams,
     };
-    use super::super::ActorMessageType;
     use super::Specific;
-    use crate::{OBJECT_LASERBEAM, TILE_HEIGHT, TILE_WIDTH};
-    use transdl::video::Surface;
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_accesscard_door_create(
         p: FnLevelActorCreateParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*p.specific) };
-
-        general.position.w = TILE_WIDTH as u16;
-        general.position.h = TILE_HEIGHT as u16;
-        general.is_in_foreground = false;
-
-        let data = Box::new(Specific {
-            tile: OBJECT_LASERBEAM,
-            current_frame: 0,
-            num_frames: 4,
-        });
-
-        *specific = Box::into_raw(data) as *mut libc::c_void;
+        p.call(super::create);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_accesscard_door_free(
         p: FnLevelActorFreeParams,
     ) {
-        unsafe {
-            if !(*p.specific).is_null() {
-                Box::from_raw(*p.specific);
-            }
-        }
+        p.call::<Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_accesscard_door_act(
         p: FnLevelActorActParams,
     ) {
-        assert!(!p.specific.is_null());
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-
-        specific.current_frame += 1;
-        specific.current_frame %= specific.num_frames;
+        p.call(super::act);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_accesscard_door_blit(
         p: FnLevelActorBlitParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        assert!(!p.tilecache.is_null());
-        assert!(!p.target.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        let tilecache = unsafe { &(*p.tilecache) };
-        let target = unsafe { &mut (*p.target) };
-
-        let mut target = Surface { raw: target };
-
-        tilecache
-            .get_tile(specific.tile + specific.current_frame)
-            .unwrap()
-            .blit_to_sdl_surface(
-                None,
-                &mut target,
-                Some(general.position),
-            );
+        p.call(super::blit);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_accesscard_door_receive_message(
         p: FnLevelActorReceiveMessageParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.level_data.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let level_data = unsafe { &mut (*p.level_data) };
-
-        if p.message != ActorMessageType::OpenDoor {
-            return;
-        }
-        let x = general.position.x as usize / TILE_WIDTH;
-        let y = general.position.y as usize / TILE_HEIGHT;
-        level_data.solids.set(x, y, false);
-        general.is_alive = false;
+        p.call(super::receive_message);
     }
 }
