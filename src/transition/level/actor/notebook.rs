08d2ff8466e039212489ecf50ec1c8706fc898fe
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-16 16:10:14 +0200

    Move notebook functionality out of ffi

--- src/transition/level/actor/notebook.rs
+++ src/transition/level/actor/notebook.rs
@@ -1,53 +1,72 @@
+use super::super::super::hero::HeroData;
+use super::super::super::infobox::InfoMessageQueue;
+use super::super::super::tilecache::TileCache;
+use super::super::LevelData;
+use super::{ActorData, ActorMessageQueue};
+use crate::{OBJECT_NOTEBOOK, TILE_HEIGHT, TILE_WIDTH};
+use transdl::video::Surface;
+
+#[derive(Debug)]
+struct Specific {}
+
+fn create(
+    general: &mut ActorData,
+    _level_data: &mut LevelData,
+) -> Specific {
+    general.position.w = TILE_WIDTH as u16;
+    general.position.h = TILE_HEIGHT as u16;
+    Specific {}
+}
+
+fn hero_interact_start(
+    _general: &mut ActorData,
+    _specific: &mut Specific,
+    _level_data: &mut LevelData,
+    _hero_data: &mut HeroData,
+    info_message_queue: &mut InfoMessageQueue,
+    _actor_message_queue: &mut ActorMessageQueue,
+) {
+    // TODO: implement functionality.
+    info_message_queue.push_back("Not implemented yet.".to_string());
+}
+
+fn blit(
+    general: &mut ActorData,
+    _specific: &mut Specific,
+    _hero_data: &mut HeroData,
+    tilecache: &TileCache,
+    target: &mut Surface,
+) {
+    tilecache
+        .get_tile(OBJECT_NOTEBOOK)
+        .unwrap()
+        .blit_to_sdl_surface(None, target, Some(general.position));
+}
+
 pub mod ffi {
     use super::super::ffi::{
         FnLevelActorBlitParams, FnLevelActorCreateParams,
         FnLevelActorHeroInteractStartParams,
     };
-    use crate::{OBJECT_NOTEBOOK, TILE_HEIGHT, TILE_WIDTH};
-    use transdl::video::Surface;
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_notebook_create(
         p: FnLevelActorCreateParams,
     ) {
-        assert!(!p.general.is_null());
-        let general = unsafe { &mut (*p.general) };
-
-        general.position.w = TILE_WIDTH as u16;
-        general.position.h = TILE_HEIGHT as u16;
+        p.call(super::create);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_notebook_hero_interact_start(
         p: FnLevelActorHeroInteractStartParams,
     ) {
-        assert!(!p.info_message_queue.is_null());
-        let info_message_queue = unsafe { &mut (*p.info_message_queue) };
-
-        // TODO: implement functionality.
-        info_message_queue.push_back("Not implemented yet.".to_string());
+        p.call(super::hero_interact_start);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_notebook_blit(
         p: FnLevelActorBlitParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.tilecache.is_null());
-        assert!(!p.target.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let tilecache = unsafe { &(*p.tilecache) };
-        let target = unsafe { &mut (*p.target) };
-
-        let mut target = Surface { raw: target };
-
-        tilecache
-            .get_tile(OBJECT_NOTEBOOK)
-            .unwrap()
-            .blit_to_sdl_surface(
-                None,
-                &mut target,
-                Some(general.position),
-            );
+        p.call(super::blit);
     }
 }
