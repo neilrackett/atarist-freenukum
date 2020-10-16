8d263a45b99eaa4599eb397e9b5dc334670c5d10
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-16 16:45:03 +0200

    Move shootable_wall functionality out of ffi

--- src/transition/level/actor/shootable_wall.rs
+++ src/transition/level/actor/shootable_wall.rs
@@ -1,82 +1,89 @@
+use super::super::super::hero::HeroData;
+use super::super::super::tilecache::TileCache;
+use super::super::LevelData;
+use super::{ActorData, ActorQueue, ActorType};
+use crate::{
+    BACKGROUND_LIGHT_GREY, SOLID_SHOOTABLE_WALL_BRICKS, TILE_HEIGHT,
+    TILE_WIDTH,
+};
+use transdl::video::Surface;
+
+#[derive(Debug)]
+struct Specific {}
+
+fn create(
+    general: &mut ActorData,
+    _level_data: &mut LevelData,
+) -> Specific {
+    general.position.w = TILE_WIDTH as u16;
+    general.position.h = TILE_HEIGHT as u16;
+    general.is_in_foreground = false;
+
+    Specific {}
+}
+
+fn shot(
+    general: &mut ActorData,
+    _specific: &mut Specific,
+    level_data: &mut LevelData,
+    actor_queue: &mut ActorQueue,
+    hero_data: &mut HeroData,
+) {
+    hero_data.score.add(10);
+    actor_queue.push_back(
+        ActorType::Explosion,
+        general.position.x as u16,
+        general.position.y as u16,
+    );
+    general.is_alive = false;
+    level_data.solids.set(
+        general.position.x as usize / TILE_WIDTH,
+        general.position.y as usize / TILE_HEIGHT,
+        false,
+    );
+}
+
+fn blit(
+    general: &mut ActorData,
+    _specific: &mut Specific,
+    _hero_data: &mut HeroData,
+    tilecache: &TileCache,
+    target: &mut Surface,
+) {
+    tilecache
+        .get_tile(BACKGROUND_LIGHT_GREY)
+        .unwrap()
+        .blit_to_sdl_surface(None, target, Some(general.position));
+    tilecache
+        .get_tile(SOLID_SHOOTABLE_WALL_BRICKS)
+        .unwrap()
+        .blit_to_sdl_surface(None, target, Some(general.position));
+}
+
 pub mod ffi {
     use super::super::ffi::{
         FnLevelActorBlitParams, FnLevelActorCreateParams,
         FnLevelActorShotParams,
     };
-    use super::super::ActorType;
-    use crate::{
-        BACKGROUND_LIGHT_GREY, SOLID_SHOOTABLE_WALL_BRICKS, TILE_HEIGHT,
-        TILE_WIDTH,
-    };
-    use transdl::video::Surface;
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_shootable_wall_create(
         p: FnLevelActorCreateParams,
     ) {
-        assert!(!p.general.is_null());
-        let general = unsafe { &mut (*p.general) };
-
-        general.position.w = TILE_WIDTH as u16;
-        general.position.h = TILE_HEIGHT as u16;
-        general.is_in_foreground = false;
+        p.call(super::create);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_shootable_wall_shot(
         p: FnLevelActorShotParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.hero_data.is_null());
-        assert!(!p.actor_queue.is_null());
-        assert!(!p.level_data.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let hero_data = unsafe { &mut (*p.hero_data) };
-        let actor_queue = unsafe { &mut (*p.actor_queue) };
-        let level_data = unsafe { &mut (*p.level_data) };
-
-        hero_data.score.add(10);
-        actor_queue.push_back(
-            ActorType::Explosion,
-            general.position.x as u16,
-            general.position.y as u16,
-        );
-        general.is_alive = false;
-        level_data.solids.set(
-            general.position.x as usize / TILE_WIDTH,
-            general.position.y as usize / TILE_HEIGHT,
-            false,
-        );
+        p.call(super::shot);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_shootable_wall_blit(
         p: FnLevelActorBlitParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.tilecache.is_null());
-        assert!(!p.target.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let tilecache = unsafe { &(*p.tilecache) };
-        let target = unsafe { &mut (*p.target) };
-
-        let mut target = Surface { raw: target };
-
-        tilecache
-            .get_tile(BACKGROUND_LIGHT_GREY)
-            .unwrap()
-            .blit_to_sdl_surface(
-                None,
-                &mut target,
-                Some(general.position),
-            );
-        tilecache
-            .get_tile(SOLID_SHOOTABLE_WALL_BRICKS)
-            .unwrap()
-            .blit_to_sdl_surface(
-                None,
-                &mut target,
-                Some(general.position),
-            );
+        p.call(super::blit);
     }
 }
