75c7f2d7f7004ae4edd75c269dfd053c560234d7
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-16 17:06:42 +0200

    Move spikes functionality out of ffi

--- src/transition/level/actor/spikes.rs
+++ src/transition/level/actor/spikes.rs
@@ -1,5 +1,68 @@
+use super::super::super::hero::HeroData;
+use super::super::super::tilecache::TileCache;
+use super::super::LevelData;
+use super::{ActorData, ActorQueue, ActorType};
+use crate::{
+    OBJECT_SPIKE, OBJECT_SPIKES_DOWN, OBJECT_SPIKES_UP, TILE_HEIGHT,
+    TILE_WIDTH,
+};
+use transdl::video::Surface;
+
+#[derive(Debug)]
 struct Specific {
     touching_hero: bool,
+}
+
+fn create(
+    general: &mut ActorData,
+    _level_data: &mut LevelData,
+) -> Specific {
+    general.position.w = TILE_WIDTH as u16;
+    general.position.h = TILE_HEIGHT as u16;
+    general.is_in_foreground = true;
+
+    Specific {
+        touching_hero: false,
+    }
+}
+
+fn hero_touch_start(
+    general: &mut ActorData,
+    specific: &mut Specific,
+    _actor_queue: &mut ActorQueue,
+    _hero_data: &mut HeroData,
+) {
+    general.hurts_hero = true;
+    specific.touching_hero = true;
+}
+
+fn hero_touch_end(
+    general: &mut ActorData,
+    specific: &mut Specific,
+    _hero_data: &mut HeroData,
+) {
+    general.hurts_hero = false;
+    specific.touching_hero = false;
+}
+
+fn blit(
+    general: &mut ActorData,
+    specific: &mut Specific,
+    _hero_data: &mut HeroData,
+    tilecache: &TileCache,
+    target: &mut Surface,
+) {
+    let tile = match general.actor_type {
+        ActorType::SpikesUp => OBJECT_SPIKES_UP,
+        ActorType::SpikesDown => OBJECT_SPIKES_DOWN,
+        ActorType::Spike if specific.touching_hero => OBJECT_SPIKE + 1,
+        ActorType::Spike => OBJECT_SPIKE,
+        _ => unreachable!(),
+    };
+
+    let tile = tilecache.get_tile(tile).unwrap();
+    let destrect = general.position;
+    tile.blit_to_sdl_surface(None, target, Some(destrect));
 }
 
 pub mod ffi {
@@ -8,94 +71,39 @@ pub mod ffi {
         FnLevelActorFreeParams, FnLevelActorHeroTouchEndParams,
         FnLevelActorHeroTouchStartParams,
     };
-    use super::super::ActorType;
-    use super::Specific;
-    use crate::{
-        OBJECT_SPIKE, OBJECT_SPIKES_DOWN, OBJECT_SPIKES_UP, TILE_HEIGHT,
-        TILE_WIDTH,
-    };
-    use transdl::video::Surface;
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_spikes_create(
         p: FnLevelActorCreateParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*p.specific) };
-
-        general.position.w = TILE_WIDTH as u16;
-        general.position.h = TILE_HEIGHT as u16;
-        general.is_in_foreground = true;
-
-        let data = Box::new(Specific {
-            touching_hero: false,
-        });
-
-        *specific = Box::into_raw(data) as *mut libc::c_void;
+        p.call(super::create);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_spikes_free(
         p: FnLevelActorFreeParams,
     ) {
-        unsafe {
-            if !(*p.specific).is_null() {
-                Box::from_raw(*p.specific);
-            }
-        }
+        p.call::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_spikes_hero_touch_start(
         p: FnLevelActorHeroTouchStartParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        general.hurts_hero = true;
-        specific.touching_hero = true;
+        p.call(super::hero_touch_start);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_spikes_hero_touch_end(
         p: FnLevelActorHeroTouchEndParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        general.hurts_hero = false;
-        specific.touching_hero = false;
+        p.call(super::hero_touch_end);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_spikes_blit(
         p: FnLevelActorBlitParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        assert!(!p.tilecache.is_null());
-        assert!(!p.target.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        let tilecache = unsafe { &(*p.tilecache) };
-        let target = unsafe { &mut (*p.target) };
-
-        let mut target = Surface { raw: target };
-
-        let tile = match general.actor_type {
-            ActorType::SpikesUp => OBJECT_SPIKES_UP,
-            ActorType::SpikesDown => OBJECT_SPIKES_DOWN,
-            ActorType::Spike if specific.touching_hero => OBJECT_SPIKE + 1,
-            ActorType::Spike => OBJECT_SPIKE,
-            _ => unreachable!(),
-        };
-
-        let tile = tilecache.get_tile(tile).unwrap();
-        let destrect = general.position;
-        tile.blit_to_sdl_surface(None, &mut target, Some(destrect));
+        p.call(super::blit);
     }
 }
