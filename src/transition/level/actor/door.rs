88afabcb2e35ffcc1647ebc69c746dba473e23f6
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-16 14:13:38 +0200

    Move door functionality out of ffi

--- src/transition/level/actor/door.rs
+++ src/transition/level/actor/door.rs
@@ -1,3 +1,10 @@
+use super::super::super::hero::HeroData;
+use super::super::super::tilecache::TileCache;
+use super::super::LevelData;
+use super::{ActorData, ActorMessageType, ActorQueue};
+use crate::{OBJECT_DOOR, TILE_HEIGHT, TILE_WIDTH};
+use transdl::video::Surface;
+
 #[derive(Debug, PartialEq, Eq)]
 enum State {
     Closed,
@@ -12,115 +19,112 @@ struct Specific {
     state: State,
 }
 
+fn create(
+    general: &mut ActorData,
+    _level_data: &mut LevelData,
+) -> Specific {
+    general.position.w = TILE_WIDTH as u16;
+    general.position.h = TILE_HEIGHT as u16;
+
+    Specific {
+        tile: OBJECT_DOOR,
+        counter: 0,
+        state: State::Closed,
+    }
+}
+
+fn act(
+    general: &mut ActorData,
+    specific: &mut Specific,
+    level_data: &mut LevelData,
+    _actor_queue: &mut ActorQueue,
+    _hero_data: &mut HeroData,
+) {
+    match specific.state {
+        State::Closed => {}
+        State::Opening => {
+            if specific.counter == 0 {
+                level_data.solids.set(
+                    general.position.x as usize / TILE_WIDTH,
+                    general.position.y as usize / TILE_HEIGHT,
+                    false,
+                );
+            }
+            specific.counter += 1;
+            if specific.counter == 8 {
+                specific.state = State::Open;
+                general.is_alive = false;
+            }
+        }
+        State::Open => {}
+    }
+}
+
+fn blit(
+    general: &mut ActorData,
+    specific: &mut Specific,
+    _hero_data: &mut HeroData,
+    tilecache: &TileCache,
+    target: &mut Surface,
+) {
+    tilecache
+        .get_tile(specific.tile + specific.counter)
+        .unwrap()
+        .blit_to_sdl_surface(None, target, Some(general.position));
+}
+
+fn receive_message(
+    _general: &mut ActorData,
+    specific: &mut Specific,
+    message: ActorMessageType,
+    _hero_data: &mut HeroData,
+    _level_data: &mut LevelData,
+) {
+    if message != ActorMessageType::OpenDoor {
+        return;
+    }
+    specific.state = State::Opening;
+}
+
 pub mod ffi {
     use super::super::ffi::{
         FnLevelActorActParams, FnLevelActorBlitParams,
         FnLevelActorCreateParams, FnLevelActorFreeParams,
         FnLevelActorReceiveMessageParams,
     };
-    use super::super::ActorMessageType;
-    use super::{Specific, State};
-    use crate::{OBJECT_DOOR, TILE_HEIGHT, TILE_WIDTH};
-    use transdl::video::Surface;
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_door_create(
         p: FnLevelActorCreateParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*p.specific) };
-
-        general.position.w = TILE_WIDTH as u16;
-        general.position.h = TILE_HEIGHT as u16;
-
-        let data = Box::new(Specific {
-            tile: OBJECT_DOOR,
-            counter: 0,
-            state: State::Closed,
-        });
-
-        *specific = Box::into_raw(data) as *mut libc::c_void;
+        p.call(super::create);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_door_free(
         p: FnLevelActorFreeParams,
     ) {
-        unsafe {
-            if !(*p.specific).is_null() {
-                Box::from_raw(*p.specific);
-            }
-        }
+        p.call::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_door_act(
         p: FnLevelActorActParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        assert!(!p.level_data.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        let level_data = unsafe { &mut (*p.level_data) };
-
-        match specific.state {
-            State::Closed => {}
-            State::Opening => {
-                if specific.counter == 0 {
-                    level_data.solids.set(
-                        general.position.x as usize / TILE_WIDTH,
-                        general.position.y as usize / TILE_HEIGHT,
-                        false,
-                    );
-                }
-                specific.counter += 1;
-                if specific.counter == 8 {
-                    specific.state = State::Open;
-                    general.is_alive = false;
-                }
-            }
-            State::Open => {}
-        }
+        p.call(super::act);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_door_blit(
         p: FnLevelActorBlitParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        assert!(!p.tilecache.is_null());
-        assert!(!p.target.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        let tilecache = unsafe { &(*p.tilecache) };
-        let target = unsafe { &mut (*p.target) };
-
-        let mut target = Surface { raw: target };
-
-        tilecache
-            .get_tile(specific.tile + specific.counter)
-            .unwrap()
-            .blit_to_sdl_surface(
-                None,
-                &mut target,
-                Some(general.position),
-            );
+        p.call(super::blit);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_door_receive_message(
         p: FnLevelActorReceiveMessageParams,
     ) {
-        assert!(!p.specific.is_null());
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-
-        if p.message != ActorMessageType::OpenDoor {
-            return;
-        }
-        specific.state = State::Opening;
+        p.call(super::receive_message);
     }
 }
