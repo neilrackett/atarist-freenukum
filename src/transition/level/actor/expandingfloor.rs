1401a7fc7b7f13fd3dd8474e7d13c8c79a7bcdb8
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-16 14:55:49 +0200

    Move expandingfloor functionality out of ffi

--- src/transition/level/actor/expandingfloor.rs
+++ src/transition/level/actor/expandingfloor.rs
@@ -1,7 +1,81 @@
+use super::super::super::hero::HeroData;
+use super::super::super::tilecache::TileCache;
+use super::super::LevelData;
+use super::ActorMessageType;
+use super::{ActorData, ActorQueue};
+use crate::{SOLID_EXPANDINGFLOOR, TILE_HEIGHT, TILE_WIDTH};
+use transdl::video::Surface;
+
 #[derive(Debug)]
 struct Specific {
     expanding: bool,
     finished: bool,
+}
+
+fn create(
+    general: &mut ActorData,
+    _level_data: &mut LevelData,
+) -> Specific {
+    general.position.w = TILE_WIDTH as u16;
+    general.position.h = TILE_HEIGHT as u16;
+
+    Specific {
+        expanding: false,
+        finished: false,
+    }
+}
+
+fn act(
+    general: &mut ActorData,
+    specific: &mut Specific,
+    level_data: &mut LevelData,
+    _actor_queue: &mut ActorQueue,
+    _hero_data: &mut HeroData,
+) {
+    if specific.expanding {
+        let x = (general.position.x as usize
+            + general.position.w as usize)
+            / TILE_WIDTH;
+        let y = general.position.y as usize / TILE_HEIGHT;
+        let can_expand = !level_data.solids.get(x, y);
+        if can_expand {
+            level_data.solids.set(x, y, true);
+            general.position.w += TILE_WIDTH as u16;
+        } else {
+            specific.expanding = false;
+            specific.finished = true;
+        }
+    }
+}
+
+fn blit(
+    general: &mut ActorData,
+    _specific: &mut Specific,
+    _hero_data: &mut HeroData,
+    tilecache: &TileCache,
+    target: &mut Surface,
+) {
+    let tile = tilecache.get_tile(SOLID_EXPANDINGFLOOR as usize).unwrap();
+    let mut destrect = general.position;
+    for _ in 0..general.position.w as usize / TILE_WIDTH {
+        tile.blit_to_sdl_surface(None, target, Some(destrect));
+        destrect.x += TILE_WIDTH as i16;
+    }
+}
+
+fn receive_message(
+    _general: &mut ActorData,
+    specific: &mut Specific,
+    message: ActorMessageType,
+    _hero_data: &mut HeroData,
+    _level_data: &mut LevelData,
+) {
+    if message != ActorMessageType::Expand {
+        return;
+    }
+    if !specific.expanding && !specific.finished {
+        specific.expanding = true;
+    }
 }
 
 pub mod ffi {
@@ -10,104 +84,39 @@ pub mod ffi {
         FnLevelActorCreateParams, FnLevelActorFreeParams,
         FnLevelActorReceiveMessageParams,
     };
-    use super::super::ActorMessageType;
-    use super::Specific;
-    use crate::{SOLID_EXPANDINGFLOOR, TILE_HEIGHT, TILE_WIDTH};
-    use transdl::video::Surface;
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_expandingfloor_create(
         p: FnLevelActorCreateParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*p.specific) };
-
-        general.position.w = TILE_WIDTH as u16;
-        general.position.h = TILE_HEIGHT as u16;
-
-        let data = Box::new(Specific {
-            expanding: false,
-            finished: false,
-        });
-
-        *specific = Box::into_raw(data) as *mut libc::c_void;
+        p.call(super::create);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_expandingfloor_free(
         p: FnLevelActorFreeParams,
     ) {
-        unsafe {
-            if !(*p.specific).is_null() {
-                Box::from_raw(*p.specific);
-            }
-        }
+        p.call::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_expandingfloor_act(
         p: FnLevelActorActParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        assert!(!p.level_data.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        let level_data = unsafe { &mut (*p.level_data) };
-
-        if specific.expanding {
-            let x = (general.position.x as usize
-                + general.position.w as usize)
-                / TILE_WIDTH;
-            let y = general.position.y as usize / TILE_HEIGHT;
-            let can_expand = !level_data.solids.get(x, y);
-            if can_expand {
-                level_data.solids.set(x, y, true);
-                general.position.w += TILE_WIDTH as u16;
-            } else {
-                specific.expanding = false;
-                specific.finished = true;
-            }
-        }
+        p.call(super::act);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_expandingfloor_blit(
         p: FnLevelActorBlitParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        assert!(!p.tilecache.is_null());
-        assert!(!p.target.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let tilecache = unsafe { &(*p.tilecache) };
-        let target = unsafe { &mut (*p.target) };
-
-        let mut target = Surface { raw: target };
-
-        let tile =
-            tilecache.get_tile(SOLID_EXPANDINGFLOOR as usize).unwrap();
-        let mut destrect = general.position;
-        for _ in 0..general.position.w as usize / TILE_WIDTH {
-            tile.blit_to_sdl_surface(None, &mut target, Some(destrect));
-            destrect.x += TILE_WIDTH as i16;
-        }
+        p.call(super::blit);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_expandingfloor_receive_message(
         p: FnLevelActorReceiveMessageParams,
     ) {
-        assert!(!p.specific.is_null());
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-
-        if p.message != ActorMessageType::Expand {
-            return;
-        }
-        if !specific.expanding && !specific.finished {
-            specific.expanding = true;
-        }
+        p.call(super::receive_message);
     }
 }
