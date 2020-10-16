5e0965bca2980b27a0bd0cd135a7fde79811f868
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-16 14:49:27 +0200

    Move exitdoor functionality out of ffi

--- src/transition/level/actor/exitdoor.rs
+++ src/transition/level/actor/exitdoor.rs
@@ -1,3 +1,11 @@
+use super::super::super::hero::HeroData;
+use super::super::super::infobox::InfoMessageQueue;
+use super::super::super::tilecache::TileCache;
+use super::super::LevelData;
+use super::{ActorData, ActorMessageQueue, ActorQueue};
+use crate::{ANIMATION_EXITDOOR, TILE_HEIGHT, TILE_WIDTH};
+use transdl::video::Surface;
+
 #[derive(PartialEq, Eq, Debug)]
 enum State {
     Closed,
@@ -12,131 +20,132 @@ struct Specific {
     state: State,
 }
 
+fn create(
+    general: &mut ActorData,
+    _level_data: &mut LevelData,
+) -> Specific {
+    general.position.w = TILE_WIDTH as u16 * 2;
+    general.position.h = TILE_HEIGHT as u16 * 2;
+    general.is_in_foreground = false;
+
+    Specific {
+        tile: ANIMATION_EXITDOOR,
+        counter: 0,
+        state: State::Closed,
+    }
+}
+
+fn hero_interact_start(
+    _general: &mut ActorData,
+    specific: &mut Specific,
+    level_data: &mut LevelData,
+    _hero_data: &mut HeroData,
+    _info_message_queue: &mut InfoMessageQueue,
+    _actor_message_queue: &mut ActorMessageQueue,
+) {
+    if specific.state == State::Closed {
+        specific.state = State::Opening;
+    }
+    level_data.level_passed = true;
+}
+
+fn act(
+    _general: &mut ActorData,
+    specific: &mut Specific,
+    level_data: &mut LevelData,
+    _actor_queue: &mut ActorQueue,
+    hero_data: &mut HeroData,
+) {
+    match specific.state {
+        State::Closed => {}
+        State::Opening => {
+            specific.counter += 1;
+            if specific.counter >= 4 {
+                hero_data.hidden = true;
+                specific.state = State::Closing;
+                specific.counter -= 1;
+            }
+        }
+        State::Closing => {
+            if specific.counter == 0 {
+                level_data.do_play = false;
+                hero_data.hidden = false;
+            } else {
+                specific.counter -= 1;
+            }
+        }
+    }
+}
+
+fn blit(
+    general: &mut ActorData,
+    specific: &mut Specific,
+    _hero_data: &mut HeroData,
+    tilecache: &TileCache,
+    target: &mut Surface,
+) {
+    let mut destrect = general.position;
+    tilecache
+        .get_tile(specific.tile + specific.counter * 4)
+        .unwrap()
+        .blit_to_sdl_surface(None, target, Some(destrect));
+    destrect.x += TILE_WIDTH as i16;
+    tilecache
+        .get_tile(specific.tile + specific.counter * 4 + 1)
+        .unwrap()
+        .blit_to_sdl_surface(None, target, Some(destrect));
+    destrect.x -= TILE_WIDTH as i16;
+    destrect.y += TILE_HEIGHT as i16;
+    tilecache
+        .get_tile(specific.tile + specific.counter * 4 + 2)
+        .unwrap()
+        .blit_to_sdl_surface(None, target, Some(destrect));
+    destrect.x += TILE_WIDTH as i16;
+    tilecache
+        .get_tile(specific.tile + specific.counter * 4 + 3)
+        .unwrap()
+        .blit_to_sdl_surface(None, target, Some(destrect));
+}
+
 pub mod ffi {
     use super::super::ffi::{
         FnLevelActorActParams, FnLevelActorBlitParams,
         FnLevelActorCreateParams, FnLevelActorFreeParams,
         FnLevelActorHeroInteractStartParams,
     };
-    use super::{Specific, State};
-    use crate::{ANIMATION_EXITDOOR, TILE_HEIGHT, TILE_WIDTH};
-    use transdl::video::Surface;
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_exitdoor_create(
         p: FnLevelActorCreateParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*p.specific) };
-
-        general.position.w = TILE_WIDTH as u16 * 2;
-        general.position.h = TILE_HEIGHT as u16 * 2;
-        general.is_in_foreground = false;
-
-        let data = Box::new(Specific {
-            tile: ANIMATION_EXITDOOR,
-            counter: 0,
-            state: State::Closed,
-        });
-
-        *specific = Box::into_raw(data) as *mut libc::c_void;
+        p.call(super::create);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_exitdoor_free(
         p: FnLevelActorFreeParams,
     ) {
-        unsafe {
-            if !(*p.specific).is_null() {
-                Box::from_raw(*p.specific);
-            }
-        }
+        p.call::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_exitdoor_hero_interact_start(
         p: FnLevelActorHeroInteractStartParams,
     ) {
-        assert!(!p.specific.is_null());
-        assert!(!p.level_data.is_null());
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        let level_data = unsafe { &mut (*p.level_data) };
-
-        if specific.state == State::Closed {
-            specific.state = State::Opening;
-        }
-        level_data.level_passed = true;
+        p.call(super::hero_interact_start);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_exitdoor_act(
         p: FnLevelActorActParams,
     ) {
-        assert!(!p.specific.is_null());
-        assert!(!p.level_data.is_null());
-        assert!(!p.hero_data.is_null());
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        let level_data = unsafe { &mut (*p.level_data) };
-        let hero_data = unsafe { &mut (*p.hero_data) };
-
-        match specific.state {
-            State::Closed => {}
-            State::Opening => {
-                specific.counter += 1;
-                if specific.counter >= 4 {
-                    hero_data.hidden = true;
-                    specific.state = State::Closing;
-                    specific.counter -= 1;
-                }
-            }
-            State::Closing => {
-                if specific.counter == 0 {
-                    level_data.do_play = false;
-                    hero_data.hidden = false;
-                } else {
-                    specific.counter -= 1;
-                }
-            }
-        }
+        p.call(super::act);
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_exitdoor_blit(
         p: FnLevelActorBlitParams,
     ) {
-        assert!(!p.general.is_null());
-        assert!(!p.specific.is_null());
-        assert!(!p.tilecache.is_null());
-        assert!(!p.target.is_null());
-        let general = unsafe { &mut (*p.general) };
-        let specific = unsafe { &mut (*(p.specific as *mut Specific)) };
-        let tilecache = unsafe { &(*p.tilecache) };
-        let target = unsafe { &mut (*p.target) };
-
-        let mut target = Surface { raw: target };
-
-        let mut destrect = general.position;
-        tilecache
-            .get_tile(specific.tile + specific.counter * 4)
-            .unwrap()
-            .blit_to_sdl_surface(None, &mut target, Some(destrect));
-        destrect.x += TILE_WIDTH as i16;
-        tilecache
-            .get_tile(specific.tile + specific.counter * 4 + 1)
-            .unwrap()
-            .blit_to_sdl_surface(None, &mut target, Some(destrect));
-        destrect.x -= TILE_WIDTH as i16;
-        destrect.y += TILE_HEIGHT as i16;
-        tilecache
-            .get_tile(specific.tile + specific.counter * 4 + 2)
-            .unwrap()
-            .blit_to_sdl_surface(None, &mut target, Some(destrect));
-        destrect.x += TILE_WIDTH as i16;
-        tilecache
-            .get_tile(specific.tile + specific.counter * 4 + 3)
-            .unwrap()
-            .blit_to_sdl_surface(None, &mut target, Some(destrect));
+        p.call(super::blit);
     }
 }
