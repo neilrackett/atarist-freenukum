55bfc9ae55ab9bdb032e39441c61f1a7f659e436
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-16 18:44:54 +0200

    Move accesscard_slot functions into ActorInterface trait

--- src/transition/level/actor/accesscard_slot.rs
+++ src/transition/level/actor/accesscard_slot.rs
@@ -3,7 +3,8 @@ use super::super::super::infobox::InfoMessageQueue;
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
 use super::{
-    ActorData, ActorMessageQueue, ActorMessageType, ActorQueue, ActorType,
+    ActorData, ActorInterface, ActorMessageQueue, ActorMessageType,
+    ActorQueue, ActorType,
 };
 use crate::{OBJECT_ACCESS_CARD_SLOT, TILE_HEIGHT, TILE_WIDTH};
 use transdl::video::Surface;
@@ -15,66 +16,68 @@ struct Specific {
     num_frames: usize,
 }
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16;
-    general.is_in_foreground = false;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Self {
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16;
+        general.is_in_foreground = false;
 
-    Specific {
-        tile: OBJECT_ACCESS_CARD_SLOT,
-        current_frame: 0,
-        num_frames: 8,
+        Specific {
+            tile: OBJECT_ACCESS_CARD_SLOT,
+            current_frame: 0,
+            num_frames: 8,
+        }
     }
-}
 
-fn hero_interact_start(
-    _general: &mut ActorData,
-    specific: &mut Specific,
-    _level_data: &mut LevelData,
-    hero_data: &mut HeroData,
-    info_message_queue: &mut InfoMessageQueue,
-    actor_message_queue: &mut ActorMessageQueue,
-) {
-    if hero_data.inventory.is_set(InventoryItem::AccessCard) {
-        actor_message_queue.push_back(
-            ActorType::AccessCardDoor,
-            ActorMessageType::OpenDoor,
-        );
-        specific.current_frame = 0;
-        specific.num_frames = 1;
-        specific.tile = OBJECT_ACCESS_CARD_SLOT + 8;
-        hero_data.inventory.unset(InventoryItem::AccessCard);
-    } else {
-        info_message_queue
-            .push_back("You don't have the access card\n".to_string());
+    fn hero_interact_start(
+        &mut self,
+        _general: &mut ActorData,
+        _level_data: &mut LevelData,
+        hero_data: &mut HeroData,
+        info_message_queue: &mut InfoMessageQueue,
+        actor_message_queue: &mut ActorMessageQueue,
+    ) {
+        if hero_data.inventory.is_set(InventoryItem::AccessCard) {
+            actor_message_queue.push_back(
+                ActorType::AccessCardDoor,
+                ActorMessageType::OpenDoor,
+            );
+            self.current_frame = 0;
+            self.num_frames = 1;
+            self.tile = OBJECT_ACCESS_CARD_SLOT + 8;
+            hero_data.inventory.unset(InventoryItem::AccessCard);
+        } else {
+            info_message_queue
+                .push_back("You don't have the access card\n".to_string());
+        }
     }
-}
 
-fn act(
-    _general: &mut ActorData,
-    specific: &mut Specific,
-    _level_data: &mut LevelData,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    specific.current_frame += 1;
-    specific.current_frame %= specific.num_frames;
-}
+    fn act(
+        &mut self,
+        _general: &mut ActorData,
+        _level_data: &mut LevelData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        self.current_frame += 1;
+        self.current_frame %= self.num_frames;
+    }
 
-fn blit(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    tilecache
-        .get_tile(specific.tile + specific.current_frame)
-        .unwrap()
-        .blit_to_sdl_surface(None, target, Some(general.position));
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        tilecache
+            .get_tile(self.tile + self.current_frame)
+            .unwrap()
+            .blit_to_sdl_surface(None, target, Some(general.position));
+    }
 }
 
 pub mod ffi {
@@ -88,34 +91,34 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_accesscard_slot_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_accesscard_slot_free(
         p: FnLevelActorFreeParams,
     ) {
-        p.call::<super::Specific>();
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_accesscard_slot_hero_interact_start(
         p: FnLevelActorHeroInteractStartParams,
     ) {
-        p.call(super::hero_interact_start);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_accesscard_slot_act(
         p: FnLevelActorActParams,
     ) {
-        p.call(super::act);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_accesscard_slot_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 }
