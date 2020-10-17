207d70a3858644ab4444b065b079c8d38ad0f58e
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-17 06:27:32 +0200

    Move camera functions into ActorInterface trait

--- src/transition/level/actor/camera.rs
+++ src/transition/level/actor/camera.rs
@@ -1,7 +1,7 @@
 use super::super::super::hero::HeroData;
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
-use super::{ActorData, ActorQueue, ActorType};
+use super::{ActorData, ActorInterface, ActorQueue, ActorType};
 use crate::{
     ANIMATION_CAMERA_CENTER, ANIMATION_CAMERA_LEFT,
     ANIMATION_CAMERA_RIGHT, TILE_HEIGHT, TILE_WIDTH,
@@ -11,63 +11,74 @@ use transdl::video::Surface;
 #[derive(Debug)]
 struct Specific {}
 
-fn create(
-    general: &mut ActorData,
-    level_data: &mut LevelData,
-) -> Specific {
-    general.is_in_foreground = false;
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        level_data: &mut LevelData,
+    ) -> Self {
+        general.is_in_foreground = false;
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16;
 
-    let x = general.position.x as usize / TILE_WIDTH;
-    let y = general.position.y as usize / TILE_HEIGHT;
-    level_data.tiles.copy_from_to(x, y + 1, x, y);
+        let x = general.position.x as usize / TILE_WIDTH;
+        let y = general.position.y as usize / TILE_HEIGHT;
+        level_data.tiles.copy_from_to(x, y + 1, x, y);
 
-    Specific {}
-}
+        Specific {}
+    }
 
-fn blit(
-    general: &mut ActorData,
-    _specific: &mut Specific,
-    hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    let x = hero_data.position.geometry.x;
-    let tile = if x - 1 > general.position.x {
-        ANIMATION_CAMERA_RIGHT
-    } else if x + 1 < general.position.x {
-        ANIMATION_CAMERA_LEFT
-    } else {
-        ANIMATION_CAMERA_CENTER
-    };
+    fn act(
+        &mut self,
+        _general: &mut ActorData,
+        _level_data: &mut LevelData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+    }
 
-    tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
-        None,
-        target,
-        Some(general.position),
-    );
-}
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        let x = hero_data.position.geometry.x;
+        let tile = if x - 1 > general.position.x {
+            ANIMATION_CAMERA_RIGHT
+        } else if x + 1 < general.position.x {
+            ANIMATION_CAMERA_LEFT
+        } else {
+            ANIMATION_CAMERA_CENTER
+        };
 
-fn shot(
-    general: &mut ActorData,
-    _specific: &mut Specific,
-    _level_data: &mut LevelData,
-    actor_queue: &mut ActorQueue,
-    hero_data: &mut HeroData,
-) {
-    general.is_alive = false;
-    hero_data.score.add(100);
-    actor_queue.push_back(
-        ActorType::Score100,
-        general.position.x as u16,
-        general.position.y as u16,
-    );
-    actor_queue.push_back(
-        ActorType::Explosion,
-        general.position.x as u16,
-        general.position.y as u16,
-    );
+        tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
+            None,
+            target,
+            Some(general.position),
+        );
+    }
+
+    fn shot(
+        &mut self,
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+        actor_queue: &mut ActorQueue,
+        hero_data: &mut HeroData,
+    ) {
+        general.is_alive = false;
+        hero_data.score.add(100);
+        actor_queue.push_back(
+            ActorType::Score100,
+            general.position.x as u16,
+            general.position.y as u16,
+        );
+        actor_queue.push_back(
+            ActorType::Explosion,
+            general.position.x as u16,
+            general.position.y as u16,
+        );
+    }
 }
 
 pub mod ffi {
@@ -80,27 +91,27 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_camera_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_camera_free(
         p: FnLevelActorFreeParams,
     ) {
-        p.call::<super::Specific>();
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_camera_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_camera_shot(
         p: FnLevelActorShotParams,
     ) {
-        p.call(super::shot);
+        p.call_interface::<super::Specific>();
     }
 }
