ff0c3ea3e1d46b6c01acd6e2b1ff64e6a5512d75
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-17 06:32:50 +0200

    Move door functions into ActorInterface trait

--- src/transition/level/actor/door.rs
+++ src/transition/level/actor/door.rs
@@ -1,7 +1,7 @@
 use super::super::super::hero::HeroData;
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
-use super::{ActorData, ActorMessageType, ActorQueue};
+use super::{ActorData, ActorInterface, ActorMessageType, ActorQueue};
 use crate::{OBJECT_DOOR, TILE_HEIGHT, TILE_WIDTH};
 use transdl::video::Surface;
 
@@ -19,71 +19,73 @@ struct Specific {
     state: State,
 }
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Specific {
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16;
 
-    Specific {
-        tile: OBJECT_DOOR,
-        counter: 0,
-        state: State::Closed,
-    }
-}
-
-fn act(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    level_data: &mut LevelData,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    match specific.state {
-        State::Closed => {}
-        State::Opening => {
-            if specific.counter == 0 {
-                level_data.solids.set(
-                    general.position.x as usize / TILE_WIDTH,
-                    general.position.y as usize / TILE_HEIGHT,
-                    false,
-                );
-            }
-            specific.counter += 1;
-            if specific.counter == 8 {
-                specific.state = State::Open;
-                general.is_alive = false;
-            }
+        Specific {
+            tile: OBJECT_DOOR,
+            counter: 0,
+            state: State::Closed,
         }
-        State::Open => {}
     }
-}
 
-fn blit(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    tilecache
-        .get_tile(specific.tile + specific.counter)
-        .unwrap()
-        .blit_to_sdl_surface(None, target, Some(general.position));
-}
-
-fn receive_message(
-    _general: &mut ActorData,
-    specific: &mut Specific,
-    message: ActorMessageType,
-    _hero_data: &mut HeroData,
-    _level_data: &mut LevelData,
-) {
-    if message != ActorMessageType::OpenDoor {
-        return;
+    fn act(
+        &mut self,
+        general: &mut ActorData,
+        level_data: &mut LevelData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        match self.state {
+            State::Closed => {}
+            State::Opening => {
+                if self.counter == 0 {
+                    level_data.solids.set(
+                        general.position.x as usize / TILE_WIDTH,
+                        general.position.y as usize / TILE_HEIGHT,
+                        false,
+                    );
+                }
+                self.counter += 1;
+                if self.counter == 8 {
+                    self.state = State::Open;
+                    general.is_alive = false;
+                }
+            }
+            State::Open => {}
+        }
     }
-    specific.state = State::Opening;
+
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        tilecache
+            .get_tile(self.tile + self.counter)
+            .unwrap()
+            .blit_to_sdl_surface(None, target, Some(general.position));
+    }
+
+    fn receive_message(
+        &mut self,
+        _general: &mut ActorData,
+        message: ActorMessageType,
+        _hero_data: &mut HeroData,
+        _level_data: &mut LevelData,
+    ) {
+        if message != ActorMessageType::OpenDoor {
+            return;
+        }
+        self.state = State::Opening;
+    }
 }
 
 pub mod ffi {
@@ -97,34 +99,34 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_door_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_door_free(
         p: FnLevelActorFreeParams,
     ) {
-        p.call::<super::Specific>();
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_door_act(
         p: FnLevelActorActParams,
     ) {
-        p.call(super::act);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_door_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_door_receive_message(
         p: FnLevelActorReceiveMessageParams,
     ) {
-        p.call(super::receive_message);
+        p.call_interface::<super::Specific>();
     }
 }
