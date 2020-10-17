b03e068d0e4008169a066c707b2bbc2cd40e859b
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-17 07:20:10 +0200

    Move singleanimation functions into ActorInterface trait

--- src/transition/level/actor/singleanimation.rs
+++ src/transition/level/actor/singleanimation.rs
@@ -1,7 +1,7 @@
 use super::super::super::hero::HeroData;
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
-use super::{ActorData, ActorQueue, ActorType};
+use super::{ActorData, ActorInterface, ActorQueue, ActorType};
 use crate::{
     ANIMATION_BOMBFIRE, ANIMATION_EXPLOSION, ANIMATION_ROBOT,
     OBJECT_DUSTCLOUD, OBJECT_STEAM, TILE_HEIGHT, TILE_WIDTH,
@@ -17,16 +17,18 @@ struct Specific {
     replaced_by: Option<ActorType>,
 }
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.is_in_foreground = false;
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Specific {
+        general.is_in_foreground = false;
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16;
 
-    let (tile, num_frames, can_hurt_hero, replaced_by) =
-        match general.actor_type {
+        let (tile, num_frames, can_hurt_hero, replaced_by) = match general
+            .actor_type
+        {
             ActorType::BombFire => (ANIMATION_BOMBFIRE, 6, true, None),
             ActorType::Explosion => (ANIMATION_EXPLOSION, 6, false, None),
             ActorType::DustCloud => (OBJECT_DUSTCLOUD, 5, false, None),
@@ -43,66 +45,67 @@ fn create(
             }
         };
 
-    Specific {
-        tile,
-        current_frame: 0,
-        num_frames,
-        can_hurt_hero,
-        replaced_by,
-    }
-}
-
-fn act(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _level_data: &mut LevelData,
-    actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    specific.current_frame += 1;
-    if specific.current_frame == specific.num_frames {
-        general.is_alive = false;
-        if let Some(successor) = specific.replaced_by {
-            actor_queue.push_back(
-                successor,
-                general.position.x as u16,
-                general.position.y as u16,
-            );
+        Specific {
+            tile,
+            current_frame: 0,
+            num_frames,
+            can_hurt_hero,
+            replaced_by,
         }
     }
-}
 
-fn hero_touch_start(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    if specific.can_hurt_hero {
-        general.hurts_hero = true;
+    fn act(
+        &mut self,
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+        actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        self.current_frame += 1;
+        if self.current_frame == self.num_frames {
+            general.is_alive = false;
+            if let Some(successor) = self.replaced_by {
+                actor_queue.push_back(
+                    successor,
+                    general.position.x as u16,
+                    general.position.y as u16,
+                );
+            }
+        }
     }
-}
 
-fn hero_touch_end(
-    general: &mut ActorData,
-    _specific: &mut Specific,
-    _hero_data: &mut HeroData,
-) {
-    general.hurts_hero = false;
-}
+    fn hero_touch_start(
+        &mut self,
+        general: &mut ActorData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        if self.can_hurt_hero {
+            general.hurts_hero = true;
+        }
+    }
 
-fn blit(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    let tile = tilecache
-        .get_tile((specific.tile + specific.current_frame) as usize)
-        .unwrap();
-    let destrect = general.position;
-    tile.blit_to_sdl_surface(None, target, Some(destrect));
+    fn hero_touch_end(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+    ) {
+        general.hurts_hero = false;
+    }
+
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        let tile = tilecache
+            .get_tile((self.tile + self.current_frame) as usize)
+            .unwrap();
+        let destrect = general.position;
+        tile.blit_to_sdl_surface(None, target, Some(destrect));
+    }
 }
 
 pub mod ffi {
@@ -116,41 +119,41 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_singleanimation_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_singleanimation_free(
         p: FnLevelActorFreeParams,
     ) {
-        p.call::<super::Specific>();
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_singleanimation_act(
         p: FnLevelActorActParams,
     ) {
-        p.call(super::act);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_singleanimation_hero_touch_start(
         p: FnLevelActorHeroTouchStartParams,
     ) {
-        p.call(super::hero_touch_start);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_singleanimation_hero_touch_end(
         p: FnLevelActorHeroTouchEndParams,
     ) {
-        p.call(super::hero_touch_end);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_singleanimation_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 }
