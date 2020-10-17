1ec2a1c2b61e73b1e7d502037ed368e3ee1df895
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-17 07:16:59 +0200

    Move shootable_wall functions into ActorInterface trait

--- src/transition/level/actor/shootable_wall.rs
+++ src/transition/level/actor/shootable_wall.rs
@@ -1,7 +1,7 @@
 use super::super::super::hero::HeroData;
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
-use super::{ActorData, ActorQueue, ActorType};
+use super::{ActorData, ActorInterface, ActorQueue, ActorType};
 use crate::{
     BACKGROUND_LIGHT_GREY, SOLID_SHOOTABLE_WALL_BRICKS, TILE_HEIGHT,
     TILE_WIDTH,
@@ -11,53 +11,64 @@ use transdl::video::Surface;
 #[derive(Debug)]
 struct Specific {}
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16;
-    general.is_in_foreground = false;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Specific {
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16;
+        general.is_in_foreground = false;
 
-    Specific {}
-}
+        Specific {}
+    }
 
-fn shot(
-    general: &mut ActorData,
-    _specific: &mut Specific,
-    level_data: &mut LevelData,
-    actor_queue: &mut ActorQueue,
-    hero_data: &mut HeroData,
-) {
-    hero_data.score.add(10);
-    actor_queue.push_back(
-        ActorType::Explosion,
-        general.position.x as u16,
-        general.position.y as u16,
-    );
-    general.is_alive = false;
-    level_data.solids.set(
-        general.position.x as usize / TILE_WIDTH,
-        general.position.y as usize / TILE_HEIGHT,
-        false,
-    );
-}
+    fn act(
+        &mut self,
+        _general: &mut ActorData,
+        _level_data: &mut LevelData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+    }
 
-fn blit(
-    general: &mut ActorData,
-    _specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    tilecache
-        .get_tile(BACKGROUND_LIGHT_GREY)
-        .unwrap()
-        .blit_to_sdl_surface(None, target, Some(general.position));
-    tilecache
-        .get_tile(SOLID_SHOOTABLE_WALL_BRICKS)
-        .unwrap()
-        .blit_to_sdl_surface(None, target, Some(general.position));
+    fn shot(
+        &mut self,
+        general: &mut ActorData,
+        level_data: &mut LevelData,
+        actor_queue: &mut ActorQueue,
+        hero_data: &mut HeroData,
+    ) {
+        hero_data.score.add(10);
+        actor_queue.push_back(
+            ActorType::Explosion,
+            general.position.x as u16,
+            general.position.y as u16,
+        );
+        general.is_alive = false;
+        level_data.solids.set(
+            general.position.x as usize / TILE_WIDTH,
+            general.position.y as usize / TILE_HEIGHT,
+            false,
+        );
+    }
+
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        tilecache
+            .get_tile(BACKGROUND_LIGHT_GREY)
+            .unwrap()
+            .blit_to_sdl_surface(None, target, Some(general.position));
+        tilecache
+            .get_tile(SOLID_SHOOTABLE_WALL_BRICKS)
+            .unwrap()
+            .blit_to_sdl_surface(None, target, Some(general.position));
+    }
 }
 
 pub mod ffi {
@@ -70,20 +81,20 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_shootable_wall_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_shootable_wall_shot(
         p: FnLevelActorShotParams,
     ) {
-        p.call(super::shot);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_shootable_wall_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 }
