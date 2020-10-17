9c421aca89ed1ab95846f046e622c8ad27e95df0
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-17 06:59:49 +0200

    Move keyhole functions into ActorInterface trait

--- src/transition/level/actor/keyhole.rs
+++ src/transition/level/actor/keyhole.rs
@@ -3,7 +3,8 @@ use super::super::super::infobox::InfoMessageQueue;
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
 use super::{
-    ActorData, ActorMessageQueue, ActorMessageType, ActorQueue, ActorType,
+    ActorData, ActorInterface, ActorMessageQueue, ActorMessageType,
+    ActorQueue, ActorType,
 };
 use crate::{
     OBJECT_KEYHOLE_BLACK, OBJECT_KEYHOLE_BLUE, OBJECT_KEYHOLE_GREEN,
@@ -17,95 +18,97 @@ struct Specific {
     counter: usize,
 }
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16;
-    general.is_in_foreground = false;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Specific {
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16;
+        general.is_in_foreground = false;
 
-    Specific {
-        tile: OBJECT_KEYHOLE_BLACK,
-        counter: 0,
+        Specific {
+            tile: OBJECT_KEYHOLE_BLACK,
+            counter: 0,
+        }
     }
-}
 
-fn act(
-    _general: &mut ActorData,
-    specific: &mut Specific,
-    _level_data: &mut LevelData,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    if specific.counter < 5 {
-        specific.counter += 1;
-        specific.counter %= 4;
+    fn act(
+        &mut self,
+        _general: &mut ActorData,
+        _level_data: &mut LevelData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        if self.counter < 5 {
+            self.counter += 1;
+            self.counter %= 4;
+        }
     }
-}
 
-fn blit(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    let tile = match (specific.counter, general.actor_type) {
-        (0, _) => specific.tile,
-        (_, ActorType::KeyholeRed) => OBJECT_KEYHOLE_RED,
-        (_, ActorType::KeyholeBlue) => OBJECT_KEYHOLE_BLUE,
-        (_, ActorType::KeyholePink) => OBJECT_KEYHOLE_PINK,
-        (_, ActorType::KeyholeGreen) => OBJECT_KEYHOLE_GREEN,
-        _ => unreachable!(),
-    };
-
-    tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
-        None,
-        target,
-        Some(general.position),
-    );
-}
-
-fn hero_interact_start(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _level_data: &mut LevelData,
-    hero_data: &mut HeroData,
-    info_message_queue: &mut InfoMessageQueue,
-    actor_message_queue: &mut ActorMessageQueue,
-) {
-    let (required_item, door_actor_type) = match general.actor_type {
-        ActorType::KeyholeRed => {
-            (InventoryItem::KeyRed, ActorType::DoorRed)
-        }
-        ActorType::KeyholeBlue => {
-            (InventoryItem::KeyBlue, ActorType::DoorBlue)
-        }
-        ActorType::KeyholePink => {
-            (InventoryItem::KeyPink, ActorType::DoorPink)
-        }
-        ActorType::KeyholeGreen => {
-            (InventoryItem::KeyGreen, ActorType::DoorGreen)
-        }
-        _ => unreachable!(),
-    };
-
-    if hero_data.inventory.is_set(required_item) {
-        actor_message_queue
-            .push_back(door_actor_type, ActorMessageType::OpenDoor);
-        specific.counter = 5;
-        hero_data.inventory.unset(required_item);
-    } else if specific.counter < 5 {
-        let color = match required_item {
-            InventoryItem::KeyRed => "red",
-            InventoryItem::KeyBlue => "blue",
-            InventoryItem::KeyPink => "pink",
-            InventoryItem::KeyGreen => "green",
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        let tile = match (self.counter, general.actor_type) {
+            (0, _) => self.tile,
+            (_, ActorType::KeyholeRed) => OBJECT_KEYHOLE_RED,
+            (_, ActorType::KeyholeBlue) => OBJECT_KEYHOLE_BLUE,
+            (_, ActorType::KeyholePink) => OBJECT_KEYHOLE_PINK,
+            (_, ActorType::KeyholeGreen) => OBJECT_KEYHOLE_GREEN,
             _ => unreachable!(),
         };
-        info_message_queue
-            .push_back(format!("You don't have the {} key.", color));
+
+        tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
+            None,
+            target,
+            Some(general.position),
+        );
+    }
+
+    fn hero_interact_start(
+        &mut self,
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+        hero_data: &mut HeroData,
+        info_message_queue: &mut InfoMessageQueue,
+        actor_message_queue: &mut ActorMessageQueue,
+    ) {
+        let (required_item, door_actor_type) = match general.actor_type {
+            ActorType::KeyholeRed => {
+                (InventoryItem::KeyRed, ActorType::DoorRed)
+            }
+            ActorType::KeyholeBlue => {
+                (InventoryItem::KeyBlue, ActorType::DoorBlue)
+            }
+            ActorType::KeyholePink => {
+                (InventoryItem::KeyPink, ActorType::DoorPink)
+            }
+            ActorType::KeyholeGreen => {
+                (InventoryItem::KeyGreen, ActorType::DoorGreen)
+            }
+            _ => unreachable!(),
+        };
+
+        if hero_data.inventory.is_set(required_item) {
+            actor_message_queue
+                .push_back(door_actor_type, ActorMessageType::OpenDoor);
+            self.counter = 5;
+            hero_data.inventory.unset(required_item);
+        } else if self.counter < 5 {
+            let color = match required_item {
+                InventoryItem::KeyRed => "red",
+                InventoryItem::KeyBlue => "blue",
+                InventoryItem::KeyPink => "pink",
+                InventoryItem::KeyGreen => "green",
+                _ => unreachable!(),
+            };
+            info_message_queue
+                .push_back(format!("You don't have the {} key.", color));
+        }
     }
 }
 
@@ -120,34 +123,34 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_keyhole_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_keyhole_free(
         p: FnLevelActorFreeParams,
     ) {
-        p.call::<super::Specific>();
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_keyhole_act(
         p: FnLevelActorActParams,
     ) {
-        p.call(super::act);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_keyhole_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_keyhole_hero_interact_start(
         p: FnLevelActorHeroInteractStartParams,
     ) {
-        p.call(super::hero_interact_start);
+        p.call_interface::<super::Specific>();
     }
 }
