26db4d8c4688996c691ed914390ef32d0b9752c8
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-17 06:54:08 +0200

    Move hostileshot functions into ActorInterface trait

--- src/transition/level/actor/hostileshot.rs
+++ src/transition/level/actor/hostileshot.rs
@@ -1,7 +1,7 @@
 use super::super::super::hero::HeroData;
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
-use super::{ActorData, ActorQueue, ActorType};
+use super::{ActorData, ActorInterface, ActorQueue, ActorType};
 use crate::{OBJECT_HOSTILESHOT, TILE_HEIGHT, TILE_WIDTH};
 use transdl::video::Surface;
 
@@ -13,90 +13,91 @@ struct Specific {
     num_frames: usize,
 }
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Specific {
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16;
 
-    let tile = match general.actor_type {
-        ActorType::HostileShotLeft => OBJECT_HOSTILESHOT,
-        ActorType::HostileShotRight => OBJECT_HOSTILESHOT + 2,
-        _ => unreachable!(
-            "Passed actor type {:?} to hostile shot actor \
+        let tile = match general.actor_type {
+            ActorType::HostileShotLeft => OBJECT_HOSTILESHOT,
+            ActorType::HostileShotRight => OBJECT_HOSTILESHOT + 2,
+            _ => unreachable!(
+                "Passed actor type {:?} to hostile shot actor \
                 which is not a shot actor id",
-            general.actor_type
-        ),
-    };
+                general.actor_type
+            ),
+        };
 
-    Specific {
-        tile,
-        touching_hero: false,
-        current_frame: 0,
-        num_frames: 2,
-    }
-}
-
-fn hero_touch_start(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    specific.touching_hero = true;
-    general.hurts_hero = true;
-}
-
-fn hero_touch_end(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-) {
-    specific.touching_hero = false;
-    general.hurts_hero = false;
-}
-
-fn act(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    level_data: &mut LevelData,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    let offset = match general.actor_type {
-        ActorType::HostileShotLeft => -(TILE_WIDTH as i16),
-        ActorType::HostileShotRight => TILE_WIDTH as i16,
-        _ => unreachable!(),
-    };
-    general.position.x += offset;
-
-    if level_data.solids.get(
-        general.position.x as usize / TILE_WIDTH,
-        general.position.y as usize / TILE_HEIGHT,
-    ) {
-        general.is_alive = false;
-        if specific.touching_hero {
-            general.hurts_hero = false;
+        Specific {
+            tile,
+            touching_hero: false,
+            current_frame: 0,
+            num_frames: 2,
         }
     }
 
-    specific.current_frame += 1;
-    specific.current_frame %= specific.num_frames;
-}
+    fn hero_touch_start(
+        &mut self,
+        general: &mut ActorData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        self.touching_hero = true;
+        general.hurts_hero = true;
+    }
 
-fn blit(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    let tile = tilecache
-        .get_tile(specific.tile + specific.current_frame)
-        .unwrap();
-    let destrect = general.position;
-    tile.blit_to_sdl_surface(None, target, Some(destrect));
+    fn hero_touch_end(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+    ) {
+        self.touching_hero = false;
+        general.hurts_hero = false;
+    }
+
+    fn act(
+        &mut self,
+        general: &mut ActorData,
+        level_data: &mut LevelData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        let offset = match general.actor_type {
+            ActorType::HostileShotLeft => -(TILE_WIDTH as i16),
+            ActorType::HostileShotRight => TILE_WIDTH as i16,
+            _ => unreachable!(),
+        };
+        general.position.x += offset;
+
+        if level_data.solids.get(
+            general.position.x as usize / TILE_WIDTH,
+            general.position.y as usize / TILE_HEIGHT,
+        ) {
+            general.is_alive = false;
+            if self.touching_hero {
+                general.hurts_hero = false;
+            }
+        }
+
+        self.current_frame += 1;
+        self.current_frame %= self.num_frames;
+    }
+
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        let tile =
+            tilecache.get_tile(self.tile + self.current_frame).unwrap();
+        let destrect = general.position;
+        tile.blit_to_sdl_surface(None, target, Some(destrect));
+    }
 }
 
 pub mod ffi {
@@ -110,41 +111,41 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_hostileshot_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_hostileshot_free(
         p: FnLevelActorFreeParams,
     ) {
-        p.call::<super::Specific>();
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_hostileshot_hero_touch_start(
         p: FnLevelActorHeroTouchStartParams,
     ) {
-        p.call(super::hero_touch_start);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_hostileshot_hero_touch_end(
         p: FnLevelActorHeroTouchEndParams,
     ) {
-        p.call(super::hero_touch_end);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_hostileshot_act(
         p: FnLevelActorActParams,
     ) {
-        p.call(super::act);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_hostileshot_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 }
