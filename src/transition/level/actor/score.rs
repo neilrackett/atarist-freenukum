befbadabd5003498854b92e3f6e30277aff2122b
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-17 07:15:23 +0200

    Move score functions into ActorInterface trait

--- src/transition/level/actor/score.rs
+++ src/transition/level/actor/score.rs
@@ -1,7 +1,7 @@
 use super::super::super::hero::HeroData;
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
-use super::{ActorData, ActorQueue, ActorType};
+use super::{ActorData, ActorInterface, ActorQueue, ActorType};
 use crate::{
     NUMBER_100, NUMBER_1000, NUMBER_10000, NUMBER_200, NUMBER_2000,
     NUMBER_500, NUMBER_5000, NUMBER_BONUS_1_LEFT, NUMBER_BONUS_1_RIGHT,
@@ -19,77 +19,79 @@ struct Specific {
     countdown: usize,
 }
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.is_in_foreground = true;
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Specific {
+        general.is_in_foreground = true;
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16;
 
-    let tile = match general.actor_type {
-        ActorType::Score100 => NUMBER_100,
-        ActorType::Score200 => NUMBER_200,
-        ActorType::Score500 => NUMBER_500,
-        ActorType::Score1000 => NUMBER_1000,
-        ActorType::Score2000 => NUMBER_2000,
-        ActorType::Score5000 => NUMBER_5000,
-        ActorType::Score10000 => NUMBER_10000,
-        ActorType::ScoreBonus1Left => NUMBER_BONUS_1_LEFT,
-        ActorType::ScoreBonus1Right => NUMBER_BONUS_1_RIGHT,
-        ActorType::ScoreBonus2Left => NUMBER_BONUS_2_LEFT,
-        ActorType::ScoreBonus2Right => NUMBER_BONUS_2_RIGHT,
-        ActorType::ScoreBonus3Left => NUMBER_BONUS_3_LEFT,
-        ActorType::ScoreBonus3Right => NUMBER_BONUS_3_RIGHT,
-        ActorType::ScoreBonus4Left => NUMBER_BONUS_4_LEFT,
-        ActorType::ScoreBonus4Right => NUMBER_BONUS_4_RIGHT,
-        ActorType::ScoreBonus5Left => NUMBER_BONUS_5_LEFT,
-        ActorType::ScoreBonus5Right => NUMBER_BONUS_5_RIGHT,
-        ActorType::ScoreBonus6Left => NUMBER_BONUS_6_LEFT,
-        ActorType::ScoreBonus6Right => NUMBER_BONUS_6_RIGHT,
-        ActorType::ScoreBonus7Left => NUMBER_BONUS_7_LEFT,
-        ActorType::ScoreBonus7Right => NUMBER_BONUS_7_RIGHT,
-        _ => {
-            unreachable!(
-                "Actor type {:?} added as an score \
+        let tile = match general.actor_type {
+            ActorType::Score100 => NUMBER_100,
+            ActorType::Score200 => NUMBER_200,
+            ActorType::Score500 => NUMBER_500,
+            ActorType::Score1000 => NUMBER_1000,
+            ActorType::Score2000 => NUMBER_2000,
+            ActorType::Score5000 => NUMBER_5000,
+            ActorType::Score10000 => NUMBER_10000,
+            ActorType::ScoreBonus1Left => NUMBER_BONUS_1_LEFT,
+            ActorType::ScoreBonus1Right => NUMBER_BONUS_1_RIGHT,
+            ActorType::ScoreBonus2Left => NUMBER_BONUS_2_LEFT,
+            ActorType::ScoreBonus2Right => NUMBER_BONUS_2_RIGHT,
+            ActorType::ScoreBonus3Left => NUMBER_BONUS_3_LEFT,
+            ActorType::ScoreBonus3Right => NUMBER_BONUS_3_RIGHT,
+            ActorType::ScoreBonus4Left => NUMBER_BONUS_4_LEFT,
+            ActorType::ScoreBonus4Right => NUMBER_BONUS_4_RIGHT,
+            ActorType::ScoreBonus5Left => NUMBER_BONUS_5_LEFT,
+            ActorType::ScoreBonus5Right => NUMBER_BONUS_5_RIGHT,
+            ActorType::ScoreBonus6Left => NUMBER_BONUS_6_LEFT,
+            ActorType::ScoreBonus6Right => NUMBER_BONUS_6_RIGHT,
+            ActorType::ScoreBonus7Left => NUMBER_BONUS_7_LEFT,
+            ActorType::ScoreBonus7Right => NUMBER_BONUS_7_RIGHT,
+            _ => {
+                unreachable!(
+                    "Actor type {:?} added as an score \
                     which is not a score id",
-                general.actor_type
-            );
+                    general.actor_type
+                );
+            }
+        };
+
+        Specific {
+            tile,
+            countdown: 40,
         }
-    };
-
-    Specific {
-        tile,
-        countdown: 40,
     }
-}
 
-fn act(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _level_data: &mut LevelData,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    specific.countdown -= 1;
-    general.position.y -= 1;
-    if specific.countdown == 0
-        || general.position.y == -(TILE_HEIGHT as i16)
-    {
-        general.is_alive = false;
+    fn act(
+        &mut self,
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        self.countdown -= 1;
+        general.position.y -= 1;
+        if self.countdown == 0
+            || general.position.y == -(TILE_HEIGHT as i16)
+        {
+            general.is_alive = false;
+        }
     }
-}
 
-fn blit(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    let tile = tilecache.get_tile(specific.tile as usize).unwrap();
-    let destrect = general.position;
-    tile.blit_to_sdl_surface(None, target, Some(destrect));
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        let tile = tilecache.get_tile(self.tile as usize).unwrap();
+        let destrect = general.position;
+        tile.blit_to_sdl_surface(None, target, Some(destrect));
+    }
 }
 
 pub mod ffi {
@@ -102,27 +104,27 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_score_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_score_free(
         p: FnLevelActorFreeParams,
     ) {
-        p.call::<super::Specific>();
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_score_act(
         p: FnLevelActorActParams,
     ) {
-        p.call(super::act);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_score_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 }
