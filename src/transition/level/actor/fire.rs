d3490eda8d24c0d52a8f3fdfe1c7f75c8ecc923b
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-17 06:45:57 +0200

    Move fire functions into ActorInterface trait

--- src/transition/level/actor/fire.rs
+++ src/transition/level/actor/fire.rs
@@ -2,7 +2,7 @@ use super::super::super::hero::HeroData;
 use super::super::super::tilecache::TileCache;
 use super::super::super::HorizontalDirection;
 use super::super::LevelData;
-use super::{ActorData, ActorQueue, ActorType};
+use super::{ActorData, ActorInterface, ActorQueue, ActorType};
 use crate::{OBJECT_FIRELEFT, OBJECT_FIRERIGHT, TILE_HEIGHT, TILE_WIDTH};
 use transdl::video::Surface;
 
@@ -22,155 +22,157 @@ struct Specific {
     touching_hero: bool,
 }
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.position.w = TILE_WIDTH as u16 * 3;
-    general.position.h = TILE_HEIGHT as u16;
-    general.is_in_foreground = true;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Specific {
+        general.position.w = TILE_WIDTH as u16 * 3;
+        general.position.h = TILE_HEIGHT as u16;
+        general.is_in_foreground = true;
 
-    let (tile, direction) = match general.actor_type {
-        ActorType::FireRight => {
-            (OBJECT_FIRERIGHT, HorizontalDirection::Right)
-        }
-        ActorType::FireLeft => {
-            general.position.x -= 2 * TILE_WIDTH as i16;
-            (OBJECT_FIRELEFT, HorizontalDirection::Left)
-        }
-        _ => unreachable!(),
-    };
-
-    Specific {
-        tile,
-        direction,
-        state: State::Off,
-        counter: 0,
-        touching_hero: false,
-    }
-}
-
-fn hero_touch_start(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    general.hurts_hero = specific.state == State::Burning;
-    specific.touching_hero = true;
-}
-
-fn hero_touch_end(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-) {
-    general.hurts_hero = false;
-    specific.touching_hero = false;
-}
-
-fn act(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _level_data: &mut LevelData,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    match specific.state {
-        State::Off => {
-            if specific.counter == 40 {
-                specific.counter = 0;
-                specific.state = State::Ignition;
+        let (tile, direction) = match general.actor_type {
+            ActorType::FireRight => {
+                (OBJECT_FIRERIGHT, HorizontalDirection::Right)
             }
-        }
-        State::Ignition => {
-            if specific.counter == 20 {
-                specific.counter = 0;
-                specific.state = State::Burning;
-                if specific.touching_hero {
-                    general.hurts_hero = true;
-                }
+            ActorType::FireLeft => {
+                general.position.x -= 2 * TILE_WIDTH as i16;
+                (OBJECT_FIRELEFT, HorizontalDirection::Left)
             }
-        }
-        State::Burning => {
-            if specific.counter == 20 {
-                specific.counter = 0;
-                specific.state = State::Off;
-                if specific.touching_hero {
-                    general.hurts_hero = false;
-                }
-            }
+            _ => unreachable!(),
+        };
+
+        Specific {
+            tile,
+            direction,
+            state: State::Off,
+            counter: 0,
+            touching_hero: false,
         }
     }
 
-    specific.counter += 1;
-}
+    fn hero_touch_start(
+        &mut self,
+        general: &mut ActorData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        general.hurts_hero = self.state == State::Burning;
+        self.touching_hero = true;
+    }
 
-fn blit(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    let (tile0, tile1, tile2) = match specific.state {
-        State::Off => (None, None, None),
-        State::Ignition => {
-            if (specific.counter % 2) > 0 {
-                match specific.direction {
-                    HorizontalDirection::Left => {
-                        (None, None, Some(specific.tile))
+    fn hero_touch_end(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+    ) {
+        general.hurts_hero = false;
+        self.touching_hero = false;
+    }
+
+    fn act(
+        &mut self,
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        match self.state {
+            State::Off => {
+                if self.counter == 40 {
+                    self.counter = 0;
+                    self.state = State::Ignition;
+                }
+            }
+            State::Ignition => {
+                if self.counter == 20 {
+                    self.counter = 0;
+                    self.state = State::Burning;
+                    if self.touching_hero {
+                        general.hurts_hero = true;
                     }
-                    HorizontalDirection::Right => {
-                        (Some(specific.tile), None, None)
+                }
+            }
+            State::Burning => {
+                if self.counter == 20 {
+                    self.counter = 0;
+                    self.state = State::Off;
+                    if self.touching_hero {
+                        general.hurts_hero = false;
                     }
+                }
+            }
+        }
+
+        self.counter += 1;
+    }
+
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        let (tile0, tile1, tile2) = match self.state {
+            State::Off => (None, None, None),
+            State::Ignition => {
+                if (self.counter % 2) > 0 {
+                    match self.direction {
+                        HorizontalDirection::Left => {
+                            (None, None, Some(self.tile))
+                        }
+                        HorizontalDirection::Right => {
+                            (Some(self.tile), None, None)
+                        }
+                        HorizontalDirection::Center => unreachable!(),
+                    }
+                } else {
+                    (None, None, None)
+                }
+            }
+            State::Burning => {
+                let offset = self.counter % 2;
+                match self.direction {
+                    HorizontalDirection::Left => (
+                        Some(self.tile + 3 + offset),
+                        Some(self.tile + 1 + offset),
+                        Some(self.tile + 1 + offset),
+                    ),
+                    HorizontalDirection::Right => (
+                        Some(self.tile + 1 + offset),
+                        Some(self.tile + 1 + offset),
+                        Some(self.tile + 3 + offset),
+                    ),
                     HorizontalDirection::Center => unreachable!(),
                 }
-            } else {
-                (None, None, None)
             }
-        }
-        State::Burning => {
-            let offset = specific.counter % 2;
-            match specific.direction {
-                HorizontalDirection::Left => (
-                    Some(specific.tile + 3 + offset),
-                    Some(specific.tile + 1 + offset),
-                    Some(specific.tile + 1 + offset),
-                ),
-                HorizontalDirection::Right => (
-                    Some(specific.tile + 1 + offset),
-                    Some(specific.tile + 1 + offset),
-                    Some(specific.tile + 3 + offset),
-                ),
-                HorizontalDirection::Center => unreachable!(),
-            }
-        }
-    };
+        };
 
-    let mut destrect = general.position;
-    if let Some(tile) = tile0 {
-        tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
-            None,
-            target,
-            Some(destrect),
-        );
-    }
-    destrect.x += TILE_WIDTH as i16;
-    if let Some(tile) = tile1 {
-        tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
-            None,
-            target,
-            Some(destrect),
-        );
-    }
-    destrect.x += TILE_WIDTH as i16;
-    if let Some(tile) = tile2 {
-        tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
-            None,
-            target,
-            Some(destrect),
-        );
+        let mut destrect = general.position;
+        if let Some(tile) = tile0 {
+            tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
+                None,
+                target,
+                Some(destrect),
+            );
+        }
+        destrect.x += TILE_WIDTH as i16;
+        if let Some(tile) = tile1 {
+            tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
+                None,
+                target,
+                Some(destrect),
+            );
+        }
+        destrect.x += TILE_WIDTH as i16;
+        if let Some(tile) = tile2 {
+            tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
+                None,
+                target,
+                Some(destrect),
+            );
+        }
     }
 }
 
@@ -185,41 +187,41 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_fire_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_fire_free(
         p: FnLevelActorFreeParams,
     ) {
-        p.call::<super::Specific>();
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_fire_hero_touch_start(
         p: FnLevelActorHeroTouchStartParams,
     ) {
-        p.call(super::hero_touch_start);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_fire_hero_touch_end(
         p: FnLevelActorHeroTouchEndParams,
     ) {
-        p.call(super::hero_touch_end);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_fire_act(
         p: FnLevelActorActParams,
     ) {
-        p.call(super::act);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_fire_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 }
