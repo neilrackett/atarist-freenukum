d7b14ffe924bdfb53be04ad53d2c98db8c1e6896
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-17 06:58:04 +0200

    Move key functions into ActorInterface trait

--- src/transition/level/actor/key.rs
+++ src/transition/level/actor/key.rs
@@ -1,7 +1,7 @@
 use super::super::super::hero::{HeroData, InventoryItem};
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
-use super::{ActorData, ActorQueue, ActorType};
+use super::{ActorData, ActorInterface, ActorQueue, ActorType};
 use crate::{
     OBJECT_KEY_BLUE, OBJECT_KEY_GREEN, OBJECT_KEY_PINK, OBJECT_KEY_RED,
     TILE_HEIGHT, TILE_WIDTH,
@@ -11,61 +11,72 @@ use transdl::video::Surface;
 #[derive(Debug)]
 pub struct Specific {}
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16;
-    general.is_in_foreground = false;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Specific {
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16;
+        general.is_in_foreground = false;
 
-    Specific {}
-}
+        Specific {}
+    }
 
-fn hero_touch_start(
-    general: &mut ActorData,
-    _specific: &mut Specific,
-    actor_queue: &mut ActorQueue,
-    hero_data: &mut HeroData,
-) {
-    let item = match general.actor_type {
-        ActorType::KeyRed => InventoryItem::KeyRed,
-        ActorType::KeyBlue => InventoryItem::KeyBlue,
-        ActorType::KeyPink => InventoryItem::KeyPink,
-        ActorType::KeyGreen => InventoryItem::KeyGreen,
-        _ => unreachable!(),
-    };
+    fn hero_touch_start(
+        &mut self,
+        general: &mut ActorData,
+        actor_queue: &mut ActorQueue,
+        hero_data: &mut HeroData,
+    ) {
+        let item = match general.actor_type {
+            ActorType::KeyRed => InventoryItem::KeyRed,
+            ActorType::KeyBlue => InventoryItem::KeyBlue,
+            ActorType::KeyPink => InventoryItem::KeyPink,
+            ActorType::KeyGreen => InventoryItem::KeyGreen,
+            _ => unreachable!(),
+        };
 
-    hero_data.inventory.set(item);
-    hero_data.score.add(1000);
-    actor_queue.push_back(
-        ActorType::Score1000,
-        general.position.x as u16,
-        general.position.y as u16,
-    );
-    general.is_alive = false;
-}
+        hero_data.inventory.set(item);
+        hero_data.score.add(1000);
+        actor_queue.push_back(
+            ActorType::Score1000,
+            general.position.x as u16,
+            general.position.y as u16,
+        );
+        general.is_alive = false;
+    }
 
-fn blit(
-    general: &mut ActorData,
-    _specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    let tile = match general.actor_type {
-        ActorType::KeyRed => OBJECT_KEY_RED,
-        ActorType::KeyBlue => OBJECT_KEY_BLUE,
-        ActorType::KeyPink => OBJECT_KEY_PINK,
-        ActorType::KeyGreen => OBJECT_KEY_GREEN,
-        _ => unreachable!(),
-    };
+    fn act(
+        &mut self,
+        _general: &mut ActorData,
+        _level_data: &mut LevelData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+    }
 
-    tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
-        None,
-        target,
-        Some(general.position),
-    );
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        let tile = match general.actor_type {
+            ActorType::KeyRed => OBJECT_KEY_RED,
+            ActorType::KeyBlue => OBJECT_KEY_BLUE,
+            ActorType::KeyPink => OBJECT_KEY_PINK,
+            ActorType::KeyGreen => OBJECT_KEY_GREEN,
+            _ => unreachable!(),
+        };
+
+        tilecache.get_tile(tile).unwrap().blit_to_sdl_surface(
+            None,
+            target,
+            Some(general.position),
+        );
+    }
 }
 
 pub mod ffi {
@@ -78,20 +89,20 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_key_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_key_hero_touch_start(
         p: FnLevelActorHeroTouchStartParams,
     ) {
-        p.call(super::hero_touch_start);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_key_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 }
