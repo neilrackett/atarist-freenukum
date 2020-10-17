58b12b1f3e308a3984000945bc84b80aa124a111
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-17 06:24:48 +0200

    Move bomb functions into ActorInterface trait

--- src/transition/level/actor/bomb.rs
+++ src/transition/level/actor/bomb.rs
@@ -1,7 +1,7 @@
 use super::super::super::hero::HeroData;
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
-use super::{ActorData, ActorQueue, ActorType};
+use super::{ActorData, ActorInterface, ActorQueue, ActorType};
 use crate::{ANIMATION_BOMB, TILE_HEIGHT, TILE_WIDTH};
 use transdl::video::Surface;
 
@@ -17,101 +17,101 @@ struct Specific {
     num_flames: usize,
 }
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Self {
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16;
 
-    Specific {
-        tile: ANIMATION_BOMB,
-        current_frame: 0,
-        num_frames: 2,
-        counter: 0,
-        explode_left: true,
-        explode_right: true,
-        explode_threshold: 12,
-        num_flames: 4,
-    }
-}
-
-fn act(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    level_data: &mut LevelData,
-    actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    specific.current_frame += 1;
-    specific.current_frame %= specific.num_frames;
-
-    specific.counter += 1;
-
-    if specific.counter < specific.explode_threshold {
-    } else if specific.counter
-        < specific.explode_threshold + specific.num_flames
-    {
-        let distance = specific.counter - specific.explode_threshold;
-        if specific.explode_left {
-            // explode to the left if possible
-            let space_is_free = !level_data.solids.get(
-                general.position.x as usize / TILE_WIDTH - distance,
-                general.position.y as usize / TILE_HEIGHT,
-            );
-            let space_has_solid_below = level_data.solids.get(
-                general.position.x as usize / TILE_WIDTH - distance,
-                general.position.y as usize / TILE_HEIGHT + 1,
-            );
-            if space_is_free && space_has_solid_below {
-                actor_queue.push_back(
-                    ActorType::BombFire,
-                    general.position.x as u16
-                        - distance as u16 * TILE_WIDTH as u16,
-                    general.position.y as u16,
-                );
-            } else {
-                specific.explode_left = false;
-            }
+        Specific {
+            tile: ANIMATION_BOMB,
+            current_frame: 0,
+            num_frames: 2,
+            counter: 0,
+            explode_left: true,
+            explode_right: true,
+            explode_threshold: 12,
+            num_flames: 4,
         }
-        if specific.explode_right {
-            // explode to the right if possible
-            let space_is_free = !level_data.solids.get(
-                general.position.x as usize / TILE_WIDTH + distance,
-                general.position.y as usize / TILE_HEIGHT,
-            );
-            let space_has_solid_below = level_data.solids.get(
-                general.position.x as usize / TILE_WIDTH + distance,
-                general.position.y as usize / TILE_HEIGHT + 1,
-            );
-            if space_is_free && space_has_solid_below {
-                actor_queue.push_back(
-                    ActorType::BombFire,
-                    general.position.x as u16
-                        + distance as u16 * TILE_WIDTH as u16,
-                    general.position.y as u16,
-                );
-            } else {
-                specific.explode_right = false;
-            }
-        }
-    } else {
-        general.is_alive = false;
     }
-}
 
-fn blit(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    if specific.counter < specific.explode_threshold {
-        tilecache
-            .get_tile(specific.tile + specific.current_frame)
-            .unwrap()
-            .blit_to_sdl_surface(None, target, Some(general.position));
+    fn act(
+        &mut self,
+        general: &mut ActorData,
+        level_data: &mut LevelData,
+        actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        self.current_frame += 1;
+        self.current_frame %= self.num_frames;
+
+        self.counter += 1;
+
+        if self.counter < self.explode_threshold {
+        } else if self.counter < self.explode_threshold + self.num_flames {
+            let distance = self.counter - self.explode_threshold;
+            if self.explode_left {
+                // explode to the left if possible
+                let space_is_free = !level_data.solids.get(
+                    general.position.x as usize / TILE_WIDTH - distance,
+                    general.position.y as usize / TILE_HEIGHT,
+                );
+                let space_has_solid_below = level_data.solids.get(
+                    general.position.x as usize / TILE_WIDTH - distance,
+                    general.position.y as usize / TILE_HEIGHT + 1,
+                );
+                if space_is_free && space_has_solid_below {
+                    actor_queue.push_back(
+                        ActorType::BombFire,
+                        general.position.x as u16
+                            - distance as u16 * TILE_WIDTH as u16,
+                        general.position.y as u16,
+                    );
+                } else {
+                    self.explode_left = false;
+                }
+            }
+            if self.explode_right {
+                // explode to the right if possible
+                let space_is_free = !level_data.solids.get(
+                    general.position.x as usize / TILE_WIDTH + distance,
+                    general.position.y as usize / TILE_HEIGHT,
+                );
+                let space_has_solid_below = level_data.solids.get(
+                    general.position.x as usize / TILE_WIDTH + distance,
+                    general.position.y as usize / TILE_HEIGHT + 1,
+                );
+                if space_is_free && space_has_solid_below {
+                    actor_queue.push_back(
+                        ActorType::BombFire,
+                        general.position.x as u16
+                            + distance as u16 * TILE_WIDTH as u16,
+                        general.position.y as u16,
+                    );
+                } else {
+                    self.explode_right = false;
+                }
+            }
+        } else {
+            general.is_alive = false;
+        }
+    }
+
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        if self.counter < self.explode_threshold {
+            tilecache
+                .get_tile(self.tile + self.current_frame)
+                .unwrap()
+                .blit_to_sdl_surface(None, target, Some(general.position));
+        }
     }
 }
 
@@ -125,27 +125,27 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_bomb_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_bomb_free(
         p: FnLevelActorFreeParams,
     ) {
-        p.call::<super::Specific>();
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_bomb_act(
         p: FnLevelActorActParams,
     ) {
-        p.call(super::act);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_bomb_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 }
