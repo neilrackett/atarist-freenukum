6e41c028a61426cf54be9143568e36a582624dd1
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-17 07:08:57 +0200

    Move redball_jumping functions into ActorInterface trait

--- src/transition/level/actor/redball_jumping.rs
+++ src/transition/level/actor/redball_jumping.rs
@@ -1,7 +1,7 @@
 use super::super::super::hero::HeroData;
 use super::super::super::tilecache::TileCache;
 use super::super::LevelData;
-use super::{ActorData, ActorQueue};
+use super::{ActorData, ActorInterface, ActorQueue};
 use crate::{ANIMATION_MINE, TILE_HEIGHT, TILE_WIDTH};
 use transdl::video::Surface;
 
@@ -12,83 +12,85 @@ struct Specific {
     base_y: u16,
 }
 
-fn create(
-    general: &mut ActorData,
-    _level_data: &mut LevelData,
-) -> Specific {
-    general.position.w = TILE_WIDTH as u16;
-    general.position.h = TILE_HEIGHT as u16;
+impl ActorInterface for Specific {
+    fn create(
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+    ) -> Specific {
+        general.position.w = TILE_WIDTH as u16;
+        general.position.h = TILE_HEIGHT as u16;
 
-    Specific {
-        tile: ANIMATION_MINE,
-        counter: 0,
-        base_y: general.position.y as u16,
+        Specific {
+            tile: ANIMATION_MINE,
+            counter: 0,
+            base_y: general.position.y as u16,
+        }
     }
-}
 
-fn hero_touch_start(
-    general: &mut ActorData,
-    _specific: &mut Specific,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    general.hurts_hero = true;
-}
+    fn hero_touch_start(
+        &mut self,
+        general: &mut ActorData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        general.hurts_hero = true;
+    }
 
-fn hero_touch_end(
-    general: &mut ActorData,
-    _specific: &mut Specific,
-    _hero_data: &mut HeroData,
-) {
-    general.hurts_hero = false;
-}
+    fn hero_touch_end(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+    ) {
+        general.hurts_hero = false;
+    }
 
-fn act(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _level_data: &mut LevelData,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    let distance = match specific.counter {
-        0 => 0,
-        1 | 11 => 16,
-        2 | 10 => 28,
-        3 | 9 => 36,
-        4 | 8 => 40,
-        5 | 7 => 41,
-        6 => 42,
-        _ => unreachable!(),
-    };
-    general.position.y = specific.base_y as i16 - distance as i16;
+    fn act(
+        &mut self,
+        general: &mut ActorData,
+        _level_data: &mut LevelData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        let distance = match self.counter {
+            0 => 0,
+            1 | 11 => 16,
+            2 | 10 => 28,
+            3 | 9 => 36,
+            4 | 8 => 40,
+            5 | 7 => 41,
+            6 => 42,
+            _ => unreachable!(),
+        };
+        general.position.y = self.base_y as i16 - distance as i16;
 
-    specific.counter += 1;
-    specific.counter %= 12;
-}
+        self.counter += 1;
+        self.counter %= 12;
+    }
 
-fn blit(
-    general: &mut ActorData,
-    specific: &mut Specific,
-    _hero_data: &mut HeroData,
-    tilecache: &TileCache,
-    target: &mut Surface,
-) {
-    let tile = tilecache.get_tile(specific.tile as usize).unwrap();
-    let destrect = general.position;
-    tile.blit_to_sdl_surface(None, target, Some(destrect));
-}
+    fn blit(
+        &mut self,
+        general: &mut ActorData,
+        _hero_data: &mut HeroData,
+        tilecache: &TileCache,
+        target: &mut Surface,
+    ) {
+        let tile = tilecache.get_tile(self.tile as usize).unwrap();
+        let destrect = general.position;
+        tile.blit_to_sdl_surface(None, target, Some(destrect));
+    }
 
-fn shot(
-    _general: &mut ActorData,
-    _specific: &mut Specific,
-    _level_data: &mut LevelData,
-    _actor_queue: &mut ActorQueue,
-    _hero_data: &mut HeroData,
-) {
-    /*
-     * We don't need to do anything, this is just to absorb
-     * the bullet when the actor is shot.
-     */
+    fn shot(
+        &mut self,
+        _general: &mut ActorData,
+        _level_data: &mut LevelData,
+        _actor_queue: &mut ActorQueue,
+        _hero_data: &mut HeroData,
+    ) {
+        /*
+         * We don't need to do anything, this is just to absorb
+         * the bullet when the actor is shot.
+         */
+    }
 }
 
 pub mod ffi {
@@ -103,48 +105,48 @@ pub mod ffi {
     pub extern "C" fn fn_level_actor_function_redball_jumping_create(
         p: FnLevelActorCreateParams,
     ) {
-        p.call(super::create);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_redball_jumping_free(
         p: FnLevelActorFreeParams,
     ) {
-        p.call::<super::Specific>();
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_redball_jumping_hero_touch_start(
         p: FnLevelActorHeroTouchStartParams,
     ) {
-        p.call(super::hero_touch_start);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_redball_jumping_hero_touch_end(
         p: FnLevelActorHeroTouchEndParams,
     ) {
-        p.call(super::hero_touch_end);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_redball_jumping_act(
         p: FnLevelActorActParams,
     ) {
-        p.call(super::act);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_redball_jumping_blit(
         p: FnLevelActorBlitParams,
     ) {
-        p.call(super::blit);
+        p.call_interface::<super::Specific>();
     }
 
     #[no_mangle]
     pub extern "C" fn fn_level_actor_function_redball_jumping_shot(
         p: FnLevelActorShotParams,
     ) {
-        p.call(super::shot);
+        p.call_interface::<super::Specific>();
     }
 }
