f8c5ed766ef6696cb112d0fda4252864a36fe804
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-10-31 15:48:51 +0100

    Move picture example to rust

---
+++ examples/picture.rs
@@ -0,0 +1,57 @@
+use anyhow::{anyhow, Result};
+use freenukum::transition::picture;
+use freenukum::transition::settings::Settings;
+use freenukum::transition::{game, sdl_surface_creation_params};
+use freenukum::{WINDOW_HEIGHT, WINDOW_WIDTH};
+use std::fs::File;
+use std::path::PathBuf;
+use structopt::StructOpt;
+use transdl::event::{Event, KeyCode};
+
+/// Show an original Duke Nukem 1 game picture.
+#[derive(StructOpt, Debug)]
+struct Arguments {
+    /// The path to the file that should be shown.
+    /// The file is usually named one of: `badguy.dn1`, `credits.dn1`,
+    /// `dn.dn1`, `duke.dn1`, `end.dn1`.
+    filename: PathBuf,
+}
+
+fn main() -> Result<()> {
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+    let args = Arguments::from_args();
+
+    let mut file = File::open(&args.filename)?;
+
+    let settings = Settings::load_or_create();
+    let mut screen = game::initialize_and_get_window(
+        WINDOW_WIDTH as i32,
+        WINDOW_HEIGHT as i32,
+        settings.fullscreen,
+        format!("Freenukum {} picture example", VERSION),
+        format!("Freenukum {} picture example", VERSION),
+    )?;
+
+    let texture_creation_params = sdl_surface_creation_params(&screen);
+    let picture = picture::load(&mut file, texture_creation_params)?;
+
+    picture.blit_to_sdl_surface(None, &mut screen, None);
+    screen.update();
+
+    'event_loop: loop {
+        match Event::wait().map_err(|e| anyhow!("{}", e))? {
+            Event::Quit
+            | Event::KeyDown {
+                key: Some(KeyCode::Escape),
+                ..
+            }
+            | Event::KeyDown {
+                key: Some(KeyCode::Q),
+                ..
+            } => break 'event_loop,
+            Event::VideoExpose => screen.update(),
+            _ => {}
+        }
+    }
+    Ok(())
+}
