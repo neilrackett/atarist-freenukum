88bc94fae1ec346fa1c6cb157b95cd36ecb0823c
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-12-13 15:50:26 +0100

    Make hostileshot act while invisible

--- src/actor/hostileshot.rs
+++ src/actor/hostileshot.rs
@@ -23,6 +23,7 @@ impl ActorCreateInterface for Specific {
         _tiles: &mut LevelTiles,
     ) -> Specific {
         general.position.resize(TILE_WIDTH, TILE_HEIGHT);
+        general.acts_while_invisible = true;
 
         let tile = match general.actor_type {
             ActorType::HostileShotLeft => OBJECT_HOSTILESHOT,
