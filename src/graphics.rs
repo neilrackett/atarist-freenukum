0a882f74bd198a645384959ad53965b59375a1c8
Wolfgang Silbermayr <wolfgang@silbermayr.at>
2020-12-18 10:46:12 +0100

    Remove obsolete SurfaceExt

--- src/graphics.rs
+++ src/graphics.rs
@@ -3,8 +3,7 @@ use anyhow::anyhow;
 use rgb::RGBA8;
 use sdl2::{
     pixels::{Color, PixelFormatEnum},
-    rect::Point,
-    render::{Canvas, RenderTarget},
+    rect::Rect,
     surface::Surface,
     ttf::{Font, FontStyle, Sdl2TtfContext},
 };
@@ -25,46 +24,6 @@ pub fn load_default_font(ttf_context: &Sdl2TtfContext) -> Result<Font> {
     Ok(font)
 }
 
-pub trait SurfaceExt {
-    fn set_data(
-        &mut self,
-        data: &[u8],
-        width: u32,
-        height: u32,
-    ) -> Result<()>;
-}
-
-impl<RT: RenderTarget> SurfaceExt for Canvas<RT> {
-    fn set_data(
-        &mut self,
-        data: &[u8],
-        width: u32,
-        height: u32,
-    ) -> Result<()> {
-        let mut iter = data.iter();
-
-        for i in 0..height {
-            for j in 0..width {
-                let red = *iter.next().unwrap();
-                let green = *iter.next().unwrap();
-                let blue = *iter.next().unwrap();
-                let opaque = *iter.next().unwrap();
-
-                let color = if opaque == 0 {
-                    Color::RGBA(0, 0, 0, 0)
-                } else {
-                    Color::RGB(red, green, blue)
-                };
-
-                self.set_draw_color(color);
-                self.draw_point(Point::new(j as i32, i as i32))
-                    .map_err(|s| anyhow!(s))?;
-            }
-        }
-        Ok(())
-    }
-}
-
 pub trait Picture: Sized {
     fn from_data(width: u32, height: u32, color: &[RGBA8])
         -> Result<Self>;
@@ -74,17 +33,20 @@ impl<'t> Picture for Surface<'t> {
     fn from_data(
         width: u32,
         height: u32,
-        color: &[RGBA8],
+        pixels: &[RGBA8],
     ) -> Result<Self> {
-        let surface =
+        let mut surface =
             Surface::new(width, height, PixelFormatEnum::RGBA8888)
                 .map_err(|s| anyhow!(s))?;
-        let mut canvas =
-            Canvas::from_surface(surface).map_err(|s| anyhow!(s))?;
-        canvas.set_draw_color(Color::RGBA(0, 0, 0, 0));
-        canvas.clear();
-        use rgb::ComponentBytes;
-        canvas.set_data(color.as_bytes(), width, height)?;
-        Ok(canvas.into_surface())
+        for i in 0..height {
+            for j in 0..width {
+                let pixel = pixels[(i * width + j) as usize];
+                let color =
+                    Color::RGBA(pixel.r, pixel.g, pixel.b, pixel.a);
+                let rect = Rect::new(j as i32, i as i32, 1, 1);
+                surface.fill_rect(rect, color).map_err(|s| anyhow!(s))?;
+            }
+        }
+        Ok(surface)
     }
 }
